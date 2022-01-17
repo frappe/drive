54b753c0c661856d4af17d059fde954d545d503e
Mitul David <mituldavid@gmail.com>
2022-01-17 15:54:20 +0530

    style: Convert indentation to tabs

--- drive/api/permissions.py
+++ drive/api/permissions.py
@@ -5,18 +5,18 @@ import frappe
 
 @frappe.whitelist()
 def grant_view_access(entity_name, user, notify=1):
-    drive_entity_doc = frappe.get_doc('Drive Entity', entity_name)
-    drive_entity_doc.share(user, notify=notify)
+	drive_entity_doc = frappe.get_doc('Drive Entity', entity_name)
+	drive_entity_doc.share(user, notify=notify)
 
 
 @frappe.whitelist()
 def grant_edit_access(entity_name, user, can_share=0, notify=1):
-    drive_entity_doc = frappe.get_doc('Drive Entity', entity_name)
-    drive_entity_doc.share(user, write=1, share=can_share, notify=notify)
+	drive_entity_doc = frappe.get_doc('Drive Entity', entity_name)
+	drive_entity_doc.share(user, write=1, share=can_share, notify=notify)
 
 
 @frappe.whitelist()
 def revoke_access(entity_name, user):
-    drive_entity_doc = frappe.get_doc('Drive Entity', entity_name)
-    drive_entity_doc.unshare(user)
+	drive_entity_doc = frappe.get_doc('Drive Entity', entity_name)
+	drive_entity_doc.unshare(user)
 
