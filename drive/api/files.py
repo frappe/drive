65aea8e737f84be872d3e5bf6b757b50ec0a909f
Mitul David <mituldavid@gmail.com>
2022-01-25 17:32:34 +0530

    feat(api): Use flat storage structure

--- drive/api/files.py
+++ drive/api/files.py
@@ -3,69 +3,109 @@
 
 import frappe
 from pathlib import Path
-from werkzeug.utils import secure_filename
+import uuid
 import mimetypes
-from contextlib import ExitStack
-from drive.utils.files import get_save_path
-from drive.locks.drive_entity_lock import DriveEntityLock
+from drive.utils.files import get_user_directory, create_user_directory
 
 @frappe.whitelist()
 def upload_file():
+	"""
+	Accept chunked file contents via a multipart upload, store the file on
+	disk, and insert a corresponding DriveEntity doc.
+
+	:param file: Request object containing uploaded chunks
+	:param parent: Document-name of the parent folder. Defaults to the user directory
+	:param chunk_index: Index of chunk present in the current upload request
+	:param total_chunk_count: Total number of chunks for the file
+	:param chunk_byte_offset: Position in the file at which the current chunk starts
+	:param total_file_size: Total size of the file in bytes
+	:raises FileExistsError: If a file with the same name already exists in the specified parent folder
+	:raises ValueError: If file is not present in the request
+	:raises ValueError: If the size of the stored file does not match the specified filesize
+	:return: DriveEntity doc once entire file has been uploaded
+	"""
+
 	file = frappe.request.files['file']
 	if not file:
 		raise ValueError('File is not present in the request')
-	parent = frappe.form_dict.parent
-	save_path = get_save_path(parent, file.filename)
 
-	current_chunk = int(frappe.form_dict.chunkindex)
-	total_chunks = int(frappe.form_dict.totalchunkcount)
+	try:
+		user_directory = get_user_directory()
+	except FileNotFoundError:
+		user_directory = create_user_directory()
 
-	if save_path.exists() and current_chunk == 0:
+	parent = frappe.form_dict.parent or user_directory.name
+	current_chunk = int(frappe.form_dict.chunk_index)
+	total_chunks = int(frappe.form_dict.total_chunk_count)
+	entity_exists = frappe.db.exists({
+		'doctype': 'Drive Entity',
+		'parent_drive_entity': parent,
+		'title': file.filename
+	})
+	save_path = Path(user_directory.path) / f'{parent}_{file.filename}'
+
+	if current_chunk == 0 and (entity_exists or save_path.exists()):
 		raise FileExistsError()
+	with open(save_path, 'ab') as f:
+		f.seek(int(frappe.form_dict.chunk_byte_offset))
+		f.write(file.stream.read())
 
-	with ExitStack() as stack:
-		if parent:
-			stack.enter_context(DriveEntityLock(parent, exclusive=False))
-		with open(save_path, 'ab') as f:
-			f.seek(int(frappe.form_dict.chunkbyteoffset))
-			f.write(file.stream.read())
-
-		if current_chunk + 1 == total_chunks:
-			file_size = save_path.stat().st_size
-			if file_size != int(frappe.form_dict.totalfilesize):
-				save_path.unlink()
-				raise ValueError('Size on disk does not match the specified filesize')
-			else:
-				mime_type, encoding = mimetypes.guess_type(save_path)
-				drive_entity_doc = frappe.get_doc({
-					'doctype': 'Drive Entity',
-					'parent_drive_entity': parent,
-					'title': secure_filename(file.filename),
-					'file_size': file_size,
-					'mime_type': mime_type
-				})
-				drive_entity_doc.flags.save_path = save_path
-				frappe.local.rollback_observers.append(drive_entity_doc)
-				drive_entity_doc.save()
-				return drive_entity_doc
+	if current_chunk + 1 == total_chunks:
+		file_size = save_path.stat().st_size
+		if file_size != int(frappe.form_dict.total_file_size):
+			save_path.unlink()
+			raise ValueError('Size on disk does not match the specified filesize')
+		else:
+			mime_type, encoding = mimetypes.guess_type(save_path)
+			name = uuid.uuid4().hex
+			path = save_path.parent / f'{name}{save_path.suffix}'
+			save_path.rename(path)
+			drive_entity = frappe.get_doc({
+				'doctype': 'Drive Entity',
+				'name': name,
+				'title': file.filename,
+				'parent_drive_entity': parent,
+				'path': path,
+				'file_size': file_size,
+				'mime_type': mime_type
+			})
+			drive_entity.flags.file_created = True
+			frappe.local.rollback_observers.append(drive_entity)
+			drive_entity.insert()
+			return drive_entity
 
 
 @frappe.whitelist()
-def create_folder(parent, folder_name):
-	save_path = get_save_path(parent, folder_name)
-	if save_path.exists():
+def create_folder(title, parent=None):
+	"""
+	Create a new folder.
+
+	:param title: Folder name
+	:param parent: Document-name of the parent folder. Defaults to the user directory
+	:raises FileExistsError: If a folder with the same name already exists in the specified parent folder
+	:return: DriveEntity doc of the new folder
+	"""
+
+	try:
+		user_directory = get_user_directory()
+	except FileNotFoundError:
+		user_directory = create_user_directory()
+
+	parent = parent or user_directory.name
+	entity_exists = frappe.db.exists({
+		'doctype': 'Drive Entity',
+		'parent_drive_entity': parent,
+		'title': title
+	})
+	if entity_exists:
 		raise FileExistsError('Folder already exists')
-	with ExitStack() as stack:
-		if parent:
-			stack.enter_context(DriveEntityLock(parent, exclusive=False))
-		save_path.mkdir()
-		drive_entity_doc = frappe.get_doc({
-			'doctype': 'Drive Entity',
-			'parent_drive_entity': parent,
-			'is_group': 1,
-			'title': secure_filename(folder_name)
-		})
-		drive_entity_doc.flags.save_path = save_path
-		frappe.local.rollback_observers.append(drive_entity_doc)
-		drive_entity_doc.save()
-		return drive_entity_doc
+
+	drive_entity = frappe.get_doc({
+		'doctype': 'Drive Entity',
+		'name': uuid.uuid4().hex,
+		'title': title,
+		'is_group': 1,
+		'parent_drive_entity': parent,
+	})
+	drive_entity.insert()
+	return drive_entity
