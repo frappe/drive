191689a309b007a184b0787f35554e8165384a0d
Mitul David <mituldavid@gmail.com>
2022-01-28 18:46:30 +0530

    feat: Get list of entities shared with the current user

--- drive/api/permissions.py
+++ drive/api/permissions.py
@@ -41,3 +41,36 @@ def get_shared_with_list(entity_name):
 		},
 		fields=['user', 'read', 'write', 'share', 'everyone', 'modified']
 	)
+
+
+@frappe.whitelist()
+def get_shared_with_me():
+	"""
+	Return the list of files and folders shared with the current user
+
+	:return: List of DriveEntities with permissions
+	:rtype: list[frappe._dict]
+	"""
+
+	DocShare = frappe.qb.DocType('DocShare')
+	DriveEntity = frappe.qb.DocType('Drive Entity')
+	query = (
+		frappe.qb.from_(DocShare)
+		.inner_join(DriveEntity)
+		.on(
+			(DocShare.share_name == DriveEntity.name) &
+			(DocShare.user == frappe.session.user)
+		)
+		.select(
+			DriveEntity.name,
+			DriveEntity.title,
+			DriveEntity.is_group,
+			DriveEntity.owner,
+			DriveEntity.modified,
+			DriveEntity.file_size,
+			DocShare.read,
+			DocShare.write,
+			DocShare.share
+		)
+	)
+	return query.run(as_dict=True)
