c78ab09cf065f3b8493a65a6f1e778ac9ded45be
Mitul David <mituldavid@gmail.com>
2022-02-22 11:58:46 +0530

    feat(ui): Progress ring

---
+++ frontend/src/components/ProgressRing.vue
@@ -0,0 +1,61 @@
+<template>
+  <svg :height="radius * 2" :width="radius * 2">
+    <circle
+      class="fill-transparent stroke-current"
+      :class="secondaryClass"
+      :cx="radius"
+      :cy="radius"
+      :r="normalizedRadius"
+      :stroke-width="stroke"
+      :stroke-dasharray="circumference"
+    />
+    <circle
+      class="fill-transparent stroke-current origin-center -rotate-90 transition-all"
+      :class="primaryClass"
+      :cx="radius"
+      :cy="radius"
+      :r="normalizedRadius"
+      :stroke-width="stroke"
+      :stroke-dasharray="circumference"
+      :stroke-dashoffset="strokeDashoffset"
+    />
+  </svg>
+</template>
+<script>
+export default {
+  name: 'ProgressRing',
+  props: {
+    progress: {
+      type: Number,
+      required: true,
+    },
+    radius: {
+      type: Number,
+      default: 24,
+    },
+    stroke: {
+      type: Number,
+      default: 4,
+    },
+    primaryClass: {
+      type: String,
+      default: 'text-green-500',
+    },
+    secondaryClass: {
+      type: String,
+      default: 'text-gray-200',
+    },
+  },
+  computed: {
+    normalizedRadius() {
+      return this.radius - this.stroke
+    },
+    circumference() {
+      return this.normalizedRadius * 2 * Math.PI
+    },
+    strokeDashoffset() {
+      return this.circumference - (this.progress / 100) * this.circumference
+    },
+  },
+}
+</script>
