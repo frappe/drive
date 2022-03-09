a303f8a01fa51f1878a7e5869936b397d4c7734a
Mitul David <mituldavid@gmail.com>
2022-03-09 10:16:59 +0530

    feat(UserSearch): Two way binding of search query

--- frontend/src/components/UserSearch.vue
+++ frontend/src/components/UserSearch.vue
@@ -1,43 +1,35 @@
 <template>
-  <div>
-    <Menu as="div" class="w-full">
-      <input
-        ref="inputElement"
-        type="text"
-        v-model="searchQuery"
-        @input="search(searchQuery)"
-        @focus="
-          () => {
-            showDropdown = true
-            search(searchQuery)
-          }
-        "
-        @blur="showDropdown = false"
-        placeholder="Add user"
-        class="w-full form-input placeholder-gray-500"
-      />
-      <MenuItems
-        static
-        v-show="showDropdown && searchResults.length > 0"
-        class="z-10 w-full max-h-64 overflow-y-auto mt-2 p-1 bg-white rounded-md shadow-lg focus:outline-none divide-y divide-gray-100 left-0 origin-top-left"
+  <Menu as="div" class="w-full">
+    <input
+      type="text"
+      v-model="searchQuery"
+      @focus="search(searchQuery) && (showDropdown = true)"
+      @blur="showDropdown = false"
+      @keydown.enter="$emit('submit', searchQuery)"
+      placeholder="Add user"
+      class="w-full form-input placeholder-gray-500"
+    />
+    <MenuItems
+      static
+      v-show="showDropdown && searchResults.length > 0"
+      class="z-10 w-full max-h-64 overflow-y-auto mt-2 p-1 bg-white rounded-md shadow-lg focus:outline-none divide-y divide-gray-100 left-0 origin-top-left"
+    >
+      <MenuItem
+        v-for="result in searchResults"
+        :key="result.value"
+        v-slot="{ active }"
       >
-        <MenuItem
-          v-for="result in searchResults"
-          :key="result.value"
-          v-slot="{ active }"
+        <button
+          :class="active ? 'bg-gray-100' : 'text-gray-900'"
+          class="flex flex-col items-start w-full px-2 py-2 text-base"
+          @mousedown="selectResult(result.value)"
         >
-          <button
-            :class="active ? 'bg-gray-100' : 'text-gray-900'"
-            class="flex flex-col items-start w-full px-2 py-2 text-base"
-            @mousedown="selectResult(result.value)"
-          >
-            <div>{{ result.value }}</div>
-            <div class="text-xs text-gray-600">{{ result.description }}</div>
-          </button>
-        </MenuItem>
-      </MenuItems>
-    </Menu>
-  </div>
+          <div>{{ result.value }}</div>
+          <div class="text-xs text-gray-600">{{ result.description }}</div>
+        </button>
+      </MenuItem>
+    </MenuItems>
+  </Menu>
 </template>
 
 <script>
@@ -53,21 +45,36 @@ export default {
     MenuItems,
     MenuItem,
   },
+  props: {
+    modelValue: {
+      type: String,
+      required: true,
+    },
+  },
+  emits: ['update:modelValue', 'submit'],
   data() {
     return {
-      searchQuery: '',
       searchResults: [],
       showDropdown: false,
     }
   },
+  computed: {
+    searchQuery: {
+      get() {
+        return this.modelValue
+      },
+      set(value) {
+        this.$emit('update:modelValue', value)
+        this.search(value)
+      },
+    },
+  },
   methods: {
     selectResult(value) {
-      let inputElement = this.$refs.inputElement
-      inputElement.value = value
-      inputElement.dispatchEvent(new Event('input'))
+      this.searchQuery = value
+      this.$emit('submit', value)
       this.showDropdown = false
     },
-    onFocus() {},
     async search(txt) {
       const headers = {
         Accept: 'application/json',
