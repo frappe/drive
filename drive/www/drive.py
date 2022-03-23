67cee816dd8446e197fadefbdd53ca45b3b8b73a
Mitul David <mituldavid@gmail.com>
2022-03-23 09:26:54 +0530

    chore: Setup production build

---
+++ drive/www/drive.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2021, mituldavid and Contributors
+# See license.txt
+
+import frappe
+
+no_cache = 1
+
+
+def get_context(context):
+	csrf_token = frappe.sessions.get_csrf_token()
+	frappe.db.commit()
+	context.csrf_token = csrf_token
