cbf1f501a013dbd15e481730680135c46229fab2
Mitul David <mituldavid@gmail.com>
2022-03-24 17:12:22 +0530

    refactor(ui): Cleanup `NoFilesSection` component

--- frontend/src/components/NoFilesSection.vue
+++ frontend/src/components/NoFilesSection.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="flex flex-col justify-between items-center text-center">
+  <div
+    class="flex h-full w-full flex-col items-center justify-center rounded-lg bg-neutral-50 text-center"
+  >
     <svg
-      xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 78 85"
-      fill="none"
-      class="stroke-2 min-w-[78px] w-[7%] h-auto pb-6"
+      class="h-auto w-1/12 min-w-[78px] fill-transparent stroke-2 pb-6"
     >
       <path
         d="M42 31H66 M42 51H66 M42 25H55 M42 45H55 M65 9V8C65 4.13401 61.866 1 58 1H8C4.13401 1 1 4.13401 1 8V66C1 69.866 4.13401 73 8 73H10 M70 12H20C16.134 12 13 15.134 13 19V77C13 80.866 16.134 84 20 84H70C73.866 84 77 80.866 77 77V19C77 15.134 73.866 12 70 12Z"
@@ -15,7 +15,22 @@
         stroke="#2D95F0"
       />
     </svg>
-    <p class="text-2xl font-semibold">You don't have any files yet</p>
-    <p class="text-base">Drop files here</p>
+    <p class="text-2xl font-semibold">{{ primaryMessage }}</p>
+    <p class="text-base">{{ secondaryMessage }}</p>
   </div>
 </template>
+<script>
+export default {
+  name: 'NoFilesSection',
+  props: {
+    primaryMessage: {
+      type: String,
+      default: "You don't have any files yet",
+    },
+    secondaryMessage: {
+      type: String,
+      default: 'Drop files here',
+    },
+  },
+}
+</script>
