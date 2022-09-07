51b73655bf3570918a7e3d93fd65e9179054fe32
krantheman <akash.akasht.tom@gmail.com>
2022-09-07 18:38:18 +0530

    fix(Prod): dirty hack for array parse error

--- frontend/src/pages/Favourites.vue
+++ frontend/src/pages/Favourites.vue
@@ -211,16 +211,7 @@ export default {
                 method: 'drive.api.files.list_folder_contents',
                 params: {
                     order_by: this.orderBy,
-                    fields: [
-                        'name',
-                        'title',
-                        'is_group',
-                        'owner',
-                        'modified',
-                        'file_size',
-                        'mime_type',
-                        'creation',
-                    ],
+                    fields: 'name,title,is_group,owner,modified,file_size,mime_type,creation',
                     is_favourite: 1,
                 },
                 onSuccess(data) {
