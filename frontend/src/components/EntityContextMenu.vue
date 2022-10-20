b3de8aeaf2653ef4d4b6483bbb21413658dd674c
krantheman <akash.akasht.tom@gmail.com>
2022-10-20 16:52:52 +0530

    fix(EntityContextMenu): z-index

--- frontend/src/components/EntityContextMenu.vue
+++ frontend/src/components/EntityContextMenu.vue
@@ -1,11 +1,11 @@
 <template>
-	<div class="bg-white rounded-xl absolute shadow-md p-2 z-50 space-y-0.5"
+	<div class="bg-white rounded-xl absolute shadow-md p-2 z-10 space-y-0.5 border"
 		:style="{ left: `${entityContext.x}px`, top: `${entityContext.y}px` }" v-if="actionItems.length > 0">
 		<div v-for="(item, index) in actionItems" :key="index"
 			class="text-sm h-7 hover:bg-gray-100 cursor-pointer rounded-lg flex px-3 items-center"
 			@click="item.handler">
 			<FeatherIcon :name="item.icon" :strokeWidth="1" class="w-4 h-4 text-gray-700 mr-3" />
-			<div class="text-gray-800 text-base">{{ item.label }}</div>
+			<div class="text-gray-800 text-small">{{ item.label }}</div>
 		</div>
 	</div>
 </template>
