c57747fbfdac4dfc07d1f6b3927dcb02e609750e
krantheman <akash.akasht.tom@gmail.com>
2023-02-13 17:24:18 +0530

    wip(EntityContextMenu): add popover for folder color change

---
+++ frontend/src/components/ColorPopover.vue
@@ -0,0 +1,19 @@
+<template>
+  <Popover trigger="hover" :hoverDelay="0.5" :leaveDelay="0.5">
+    <template #target>
+      <Button>Hover me</Button>
+    </template>
+    <template #body-main>
+      <div class="p-2 text-base">Popup content</div>
+    </template>
+  </Popover>
+</template>
+
+<script>
+import { Popover, Button } from 'frappe-ui';
+
+export default {
+  name: 'ColorPopover',
+  components: { Popover, Button },
+};
+</script>
--- frontend/src/components/EntityContextMenu.vue
+++ frontend/src/components/EntityContextMenu.vue
@@ -1,32 +1,74 @@
 <template>
-	<div class="bg-white rounded-xl absolute shadow-md p-2 z-10 space-y-0.5 border"
-		:style="{ left: `${entityContext.x}px`, top: `${entityContext.y}px` }" v-if="actionItems.length > 0">
-		<div v-for="(item, index) in actionItems" :key="index"
-			class="text-sm h-7 hover:bg-gray-100 cursor-pointer rounded-lg flex px-3 items-center" @click="() => {
-				item.handler()
-				close()
-			}">
-			<FeatherIcon :name="item.icon" class="stroke-1.5  w-4 h-4 text-gray-700 mr-3" />
-			<div class="text-gray-800 text-small">{{ item.label }}</div>
-		</div>
-	</div>
+  <div
+    class="bg-white rounded-xl absolute shadow-md p-2 z-10 space-y-0.5 border"
+    :style="{ left: `${entityContext.x}px`, top: `${entityContext.y}px` }"
+    v-if="actionItems.length > 0"
+  >
+    <div
+      v-for="(item, index) in actionItems"
+      :key="index"
+      class="text-sm"
+      @click="
+        () => {
+          item.handler();
+          close();
+        }
+      "
+    >
+      <Popover
+        v-if="item.label === 'Change Color'"
+        trigger="hover"
+        placement="right-start"
+        :hoverDelay="0.5"
+        :leaveDelay="0.5"
+        class="hover:bg-gray-100 cursor-pointer rounded-lg flex px-3"
+      >
+        <template #target>
+          <div class="h-7 flex items-center">
+            <FeatherIcon
+              :name="item.icon"
+              class="stroke-1.5 w-4 h-4 text-gray-700 mr-3"
+            />
+            <div class="text-gray-800">{{ item.label }}</div>
+            <FeatherIcon
+              :name="'chevron-right'"
+              class="w-4 h-4 text-gray-700 ml-20"
+            />
+          </div>
+        </template>
+        <template #body-main>
+          <div class="p-2 text-base">Popup content</div>
+        </template>
+      </Popover>
+      <div
+        v-else
+        class="h-7 hover:bg-gray-100 cursor-pointer rounded-lg flex px-3 items-center"
+      >
+        <FeatherIcon
+          :name="item.icon"
+          class="stroke-1.5 w-4 h-4 text-gray-700 mr-3"
+        />
+        <div class="text-gray-800">{{ item.label }}</div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-import { FeatherIcon } from 'frappe-ui'
+import { FeatherIcon, Popover, Button } from 'frappe-ui';
 
 export default {
-	name: 'EntityContextMenu',
-	components: { FeatherIcon },
-	props: {
-		actionItems: {
-			type: Array,
-		},
-		entityContext: {
-			type: Object,
-		},
-		close: {
-			type: Function,
-		},
-	},
-}
+  name: 'EntityContextMenu',
+  components: { FeatherIcon, Popover, Button },
+  props: {
+    actionItems: {
+      type: Array,
+    },
+    entityContext: {
+      type: Object,
+    },
+    close: {
+      type: Function,
+    },
+  },
+};
 </script>
