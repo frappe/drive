e66b5931b1a0d1e75b0d54ff7141490a34108e5c
Arjun Choudhary <contact@arjunchoudhary.com>
2023-03-06 16:13:42 +0530

    fix: Dev server proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,24 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
-import { getProxyOptions } from 'frappe-ui/src/utils/vite-dev-server';
 import { webserver_port } from '../../../sites/common_site_config.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
+    host: '127.0.0.1',
     port: 8080,
-    proxy: getProxyOptions({ port: webserver_port }),
+    proxy: {
+      '^/(drive|app|login|api|assets|files)': {
+        target: `http://127.0.0.1:${webserver_port}`,
+        ws: true,
+        router: function (req) {
+          const site_name = req.headers.host.split(':')[0]
+          return `http://${site_name}:${webserver_port}`
+        },
+      },
+    },
   },
   resolve: {
     alias: {
