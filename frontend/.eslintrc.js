f0981323ab85aa37e35e933c6141fd1c27f2dd72
Arjun Choudhary <contact@arjunchoudhary.com>
2023-03-09 14:03:52 +0530

    chore: lint rule

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -7,5 +7,6 @@ module.exports = {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
     "vue/require-default-prop": "off",
+    "vue/multi-word-component-names": "off",
   },
 };
