45900ea88e53c2be84919b2f5d38efde114453a1
Arun Mathai <me@arunmathaisk.in>
2023-06-16 13:16:58 +0530

    fix :  trash edge case for file and folder sharing solved

--- drive/api/files.py
+++ drive/api/files.py
@@ -312,7 +312,15 @@ def list_folder_contents(entity_name=None, order_by="modified", is_active=1):
             "Cannot access folder due to insufficient permissions",
             frappe.PermissionError,
         )
-
+    entity_ancestors = get_ancestors_of("Drive Entity", entity_name)
+    flag = False
+    for z_entity_name in entity_ancestors:
+        result = frappe.db.exists("Drive Entity", {"name":z_entity_name,"is_active": 0})
+        if result:
+            flag = True
+            break
+    if flag == True:
+        frappe.throw("Parent Folder has been deleted")                     
     DriveEntity = frappe.qb.DocType("Drive Entity")
     DriveFavourite = frappe.qb.DocType("Drive Favourite")
     DocShare = frappe.qb.DocType("DocShare")
