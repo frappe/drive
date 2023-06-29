5e065d19890093bcb7a5f0f8ef2422a2916bd62d
Arun Mathai <me@arunmathaisk.in>
2023-06-29 12:56:10 +0530

    fix: async behavior achieved for folder download

--- frontend/src/utils/folderDownload.js
+++ frontend/src/utils/folderDownload.js
@@ -1,78 +1,94 @@
 import JSZip from 'jszip';
 
-export function folderDownload(root_entity)
-{
+export function folderDownload(root_entity) {
+  const folderName = root_entity.title;
+  const zip = new JSZip();
 
-    var folderName = root_entity.title;
+  temp(root_entity.name, zip)
+    .then(() => {
+      return zip.generateAsync({ type: 'blob' });
+    })
+    .then((content) => {
+      const downloadLink = document.createElement('a');
+      downloadLink.href = URL.createObjectURL(content);
+      downloadLink.download = folderName + '.zip';
 
-    var zip = new JSZip();
-
-    temp(root_entity.name,zip);
-
-    zip.generateAsync({ type: 'blob' })
-      .then(function (content) {
-        // Create a download link
-        var downloadLink = document.createElement('a');
-        downloadLink.href = URL.createObjectURL(content);
-        downloadLink.download = folderName + '.zip';
-
-        // Append the link to the document body and trigger the download
-        document.body.appendChild(downloadLink);
-        downloadLink.click();
-        document.body.removeChild(downloadLink);
-      });
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      document.body.removeChild(downloadLink);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
 
-function temp(entity_name,parentZip)
-{
-    let result = get_children(entity_name);
-    result.forEach((entity) => {
-        let current_entity = entity;
-        console.log(current_entity.title);
-        if (current_entity.is_group)
-        {
-           var folder = parentZip.folder(current_entity.title);
-           temp(current_entity.name,folder);
-        }
-        else{
-            console.log("sucess.....")
-            parentZip.file(current_entity.title, get_file_content(current_entity.name));
-            
-        }
-       
+function temp(entity_name, parentZip) {
+  return new Promise((resolve, reject) => {
+    get_children(entity_name)
+      .then((result) => {
+        const promises = result.map((entity) => {
+          if (entity.is_group) {
+            const folder = parentZip.folder(entity.title);
+            return temp(entity.name, folder);
+          } else {
+            console.log("success...");
+            return get_file_content(entity.name)
+              .then((fileContent) => {
+                parentZip.file(entity.title, fileContent);
+              });
+          }
+        });
+
+        Promise.all(promises)
+          .then(() => {
+            resolve();
+          })
+          .catch((error) => {
+            reject(error);
+          });
+      })
+      .catch((error) => {
+        reject(error);
       });
+  });
 }
 
-function get_file_content(entity_name)
-{
-    var fileUrl = window.location.origin +
-            "/api/method/" +
-            `drive.api.files.get_file_content?entity_name=${entity_name}`;
-            const xhr = new XMLHttpRequest();
-            xhr.open("GET",fileUrl, false); // Here i am seeting third parameter as false for a synchronous request
-            xhr.send();
-          
-            if (xhr.status === 200) {
-              return xhr.response;
-            } else {
-              throw new Error(`Request failed with status ${xhr.status}`);
-            }
+function get_file_content(entity_name) {
+  return new Promise((resolve, reject) => {
+    const fileUrl = window.location.origin +
+      "/api/method/" +
+      `drive.api.files.get_file_content?entity_name=${entity_name}`;
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", fileUrl);
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        resolve(xhr.response);
+      } else {
+        reject(new Error(`Request failed with status ${xhr.status}`));
+      }
+    };
+    xhr.send();
+  });
 }
 
 function get_children(entity_name) {
+  return new Promise((resolve, reject) => {
     const url =
       window.location.origin +
       "/api/method/" +
       `drive.api.nested_folder.folder_contents?entity_name=${entity_name}`;
-  
+
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", url, false); // Here i am seeting third parameter as false for a synchronous request
+    xhr.open("GET", url);
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        const json = JSON.parse(xhr.responseText);
+        resolve(json.message);
+      } else {
+        reject(new Error(`Request failed with status ${xhr.status}`));
+      }
+    };
     xhr.send();
-  
-    if (xhr.status === 200) {
-      const json = JSON.parse(xhr.responseText);
-      return json.message;
-    } else {
-      throw new Error(`Request failed with status ${xhr.status}`);
-    }
-  }
+  });
+}
