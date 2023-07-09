52e9159ffdca5a4974435fc5ef268679e45a2f68
Arun Mathai <me@arunmathaisk.in>
2023-07-10 00:57:29 +0530

    fix: looks more like g-drive thumbnails, fill to div

--- frontend/src/components/File.vue
+++ frontend/src/components/File.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="h-32 place-items-center grid">
-    <img :src="link" class="h-14" :draggable="false" />
+    <img :src="link" class="h-32 w-full object-cover" :draggable="false" />
   </div>
   <div class="px-3.5 h-16 content-center grid">
     <h3 class="truncate text-[14px] font-medium">{{ this.title }}</h3>
