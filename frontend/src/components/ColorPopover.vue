3721f7a2e2a342d0c62e9d96916dc70563478093
Arjun Choudhary <contact@arjunchoudhary.com>
2023-08-01 22:41:50 +0530

    design: misc tiny style fixes

--- frontend/src/components/ColorPopover.vue
+++ frontend/src/components/ColorPopover.vue
@@ -7,7 +7,7 @@
     <template #target="{ togglePopover, isOpen }">
       <div
         :active="isOpen"
-        class="h-7 flex items-center hover:bg-gray-100 rounded-lg px-3 cursor-pointer"
+        class="h-7 flex items-center hover:bg-gray-100 rounded px-3 cursor-pointer"
         @click="togglePopover()">
         <FeatherIcon
           name="droplet"
--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -4,32 +4,30 @@
     ondragstart="return false;"
     ondrop="return false;"
     class="flex flex-col justify-between">
-    <div class="text-sm">
-      <router-link
-        v-for="item in sidebarItems"
-        :key="item.label"
-        v-slot="{ href, navigate }"
-        :to="item.route">
-        <a
-          :class="[
-            item.highlight()
-              ? 'bg-white shadow-sm text-gray-900'
-              : 'text-gray-800 hover:bg-gray-100',
-          ]"
-          :href="href"
-          class="w-full h-7 px-2 py-1 gap-3 rounded focus:outline-none flex items-center"
-          @click="navigate && $emit('toggleMobileSidebar')">
-          <FeatherIcon :name="item.icon" class="stroke-1.5 w-4 h-4" />
-          {{ item.label }}
-        </a>
-      </router-link>
-      <span
-        class="w-[256px] h-7 px-3 py-4 gap-3 mt-auto rounded-lg focus:outline-none flex items-center">
-        <FeatherIcon name="cloud" class="stroke-1.5 w-4 h-4" />
-        Used :
-        {{ $resources.getRootFolderSize.data + "B" }}
-      </span>
-    </div>
+    <router-link
+      v-for="item in sidebarItems"
+      :key="item.label"
+      v-slot="{ href, navigate }"
+      :to="item.route">
+      <a
+        :class="[
+          item.highlight()
+            ? 'bg-white shadow-sm text-gray-900'
+            : 'text-gray-800 hover:bg-gray-100',
+        ]"
+        :href="href"
+        class="text-sm w-full mb-0.5 h-7 px-2 py-1 gap-3 rounded focus:outline-none flex items-center"
+        @click="navigate && $emit('toggleMobileSidebar')">
+        <FeatherIcon :name="item.icon" class="stroke-1.5 w-4 h-4" />
+        {{ item.label }}
+      </a>
+    </router-link>
+    <span
+      class="w-[256px] h-7 px-3 py-4 gap-3 mt-auto rounded-lg focus:outline-none flex items-center">
+      <FeatherIcon name="cloud" class="stroke-1.5 w-4 h-4" />
+      Used :
+      {{ $resources.getRootFolderSize.data + "B" }}
+    </span>
   </div>
 </template>
 <script>
