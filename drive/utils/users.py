e46adf196b7997b0f1085b87d407cdee46f22e3a
Arun Mathai <me@arunmathaisk.in>
2023-08-15 09:45:40 +0530

    update: new utility functions

--- drive/utils/users.py
+++ drive/utils/users.py
@@ -11,16 +11,17 @@ def get_users_with_drive_user_role(txt=""):
     try:
         drive_users = frappe.get_all(
             doctype="User",
-            filters=[["Has Role", "role", "=", "Drive User"],["full_name", "like", f"%{txt}%"]],
+            filters=[
+                ["Has Role", "role", "=", "Drive User"],
+                ["full_name", "like", f"%{txt}%"],
+            ],
             fields=[
                 "email",
                 "full_name",
             ],
         )
-        if(len(drive_users)==0):
-            empty_results = {
-                "results": drive_users
-            }
+        if len(drive_users) == 0:
+            empty_results = {"results": drive_users}
             return empty_results
         renamed_users = []
         for user in drive_users:
@@ -29,13 +30,37 @@ def get_users_with_drive_user_role(txt=""):
                 "description": user.full_name,
             }
             renamed_users.append(renamed_user)
-            custom_response= {
-                "results": renamed_users
-            }
+            custom_response = {"results": renamed_users}
         return custom_response
     except Exception as e:
         frappe.log_error("Error in fetching Drive Users: {0}".format(e))
         return {
             "status": "error",
-            "message": "An error occurred while fetching Drive Users"
+            "message": "An error occurred while fetching Drive Users",
+        }
+
+
+@frappe.whitelist()
+def get_all_users_on_site():
+    try:
+        if not frappe.session.user == "Administrator":
+            return {
+                "status": "error",
+                "message": "You do not have permission to access this resource",
+            }
+
+        site_users = frappe.get_all(
+            doctype="User",
+            fields=[
+                "username",
+                "email",
+                "full_name",
+            ],
+        )
+        return site_users
+    except Exception as e:
+        frappe.log_error("Error in fetching Drive Users: {0}".format(e))
+        return {
+            "status": "error",
+            "message": "An error occurred while fetching Drive Users",
         }
