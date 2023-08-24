b3673d48065f3dec3dd93e7fc8371a7e4be04496
Arun Mathai <me@arunmathaisk.in>
2023-08-24 22:40:12 +0530

    feat: add singular user to group

--- drive/utils/user_group.py
+++ drive/utils/user_group.py
@@ -1,25 +1,52 @@
 import frappe
 
+
 @frappe.whitelist()
 def create_user_group(group_name, members=[]):
     try:
         # Create a new User Group
-        new_group = frappe.get_doc({
-            "doctype": "User Group",
-            "name": group_name,
-            "user_group_members": [{"user": member} for member in members]
-        })
-        
+        new_group = frappe.get_doc(
+            {
+                "doctype": "User Group",
+                "name": group_name,
+                "user_group_members": [{"user": member} for member in members],
+            }
+        )
+
         new_group.insert(ignore_permissions=True)
 
         return {
-            "message":("User Group created successfully."),
-            "user_group": new_group.name
+            "message": ("User Group created successfully."),
+            "user_group": new_group.name,
         }
-    
+
     except Exception as e:
-        frappe.log_error(title="User Group Creation Failed", message=str(e))
+        return {"message": ("User Group creation failed."), "error": str(e)}
+
+
+@frappe.whitelist()
+def add_user_to_group(group_name, user_email):
+    try:
+        # Get the User Group document
+        user_group = frappe.get_doc("User Group", group_name)
+
+        # Check if the user is already in the group
+        existing_members = [member.user for member in user_group.user_group_members]
+        if user_email in existing_members:
+            return {
+                "message": ("User already exists in the group.")
+            }
+        
+        # Add the user to the group
+        user_group.append("user_group_members", {"user": user_email})
+        user_group.save(ignore_permissions=True)
+
         return {
-            "message": ("User Group creation failed."),
+            "message": ("User added to the group successfully."),
+            "user_group": user_group.name
+        }
+    except Exception as e:
+        return {
+            "message": ("Failed to add user to the group."),
             "error": str(e)
         }
