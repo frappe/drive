f440784b567e7fbd5eaef2b911130e1cb20d6931
Arjun Choudhary <contact@arjunchoudhary.com>
2023-09-03 20:49:38 +0530

    fix: correct oauth redirect

--- drive/api/api.py
+++ drive/api/api.py
@@ -32,7 +32,7 @@ def oauth_providers():
 				{
 					"name": provider.name,
 					"provider_name": provider.provider_name,
-					"auth_url": get_oauth2_authorize_url(provider.name, "/drive"),
+					"auth_url": get_oauth2_authorize_url(provider.name, "/drive/home"),
 					"icon": icon,
 				}
 			)
