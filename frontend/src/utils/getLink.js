e0afadb976efac86fada66c4613c17ef38ca23e1
Arjun Choudhary <contact@arjunchoudhary.com>
2023-09-29 16:44:00 +0000

    refactor: toasts style

---
+++ frontend/src/utils/getLink.js
@@ -0,0 +1,13 @@
+import { toast } from "../utils/toasts.js";
+
+export async function getLink(entity) {
+  const link = entity.is_group
+    ? `${window.location.origin}/drive/folder/${entity.name}`
+    : `${window.location.origin}/drive/file/${entity.name}`;
+  await navigator.clipboard.writeText(link);
+  toast({
+    title: "Copied link!",
+    position: "bottom-right",
+    timeout: 2,
+  });
+}
