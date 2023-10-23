ff699b6f404f625c47b6a2b4da242e927f894a42
Arjun Choudhary <contact@arjunchoudhary.com>
2023-10-23 12:08:45 +0000

    chore: cache org-name on fetch

--- frontend/src/components/ShareDialog.vue
+++ frontend/src/components/ShareDialog.vue
@@ -492,8 +492,8 @@ export default {
       }
       if (this.generalAccess.everyone) {
         return this.generalAccess.write
-          ? `All users in ${this.$resources.getOrgName.data.org_name} can edit`
-          : `All users in ${this.$resources.getOrgName.data.org_name} can view`;
+          ? `All users in ${this.$resources.getOrgName?.data.org_name} can edit`
+          : `All users in ${this.$resources.getOrgName?.data.org_name} can view`;
       } else {
         return "Only users and groups with access can view or edit";
       }
@@ -700,11 +700,9 @@ export default {
       return {
         url: "frappe.client.get",
         method: "GET",
+        cache: "org_info",
         params: {
           doctype: "Drive Instance Settings",
-        },
-        onSuccess(data) {
-          console.log(data);
         },
         onError(error) {
           if (error.messages) {
