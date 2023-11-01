fd1fc6a400920dbcb25a29ac04a25d0ed3dcb47f
Arjun Choudhary <contact@arjunchoudhary.com>
2023-11-01 10:13:54 +0530

    fix: /document link copy

--- frontend/src/utils/getLink.js
+++ frontend/src/utils/getLink.js
@@ -3,6 +3,8 @@ import { toast } from "../utils/toasts.js";
 export async function getLink(entity) {
   const link = entity.is_group
     ? `${window.location.origin}/drive/folder/${entity.name}`
+    : entity.document
+    ? `${window.location.origin}/drive/document/${entity.name}`
     : `${window.location.origin}/drive/file/${entity.name}`;
   await navigator.clipboard.writeText(link);
   toast({
