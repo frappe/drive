107d7d264d3b0d54b539242830b043e0f39a09a8
Arjun Choudhary <contact@arjunchoudhary.com>
2024-01-15 05:29:05 +0000

    chore:  parity

--- drive/overrides.py
+++ drive/overrides.py
@@ -7,7 +7,7 @@ def user_has_permission(doc, ptype, user):
         user = frappe.session.user
 
     if doc.owner == user:
-        return
+        return True
 
     if user != "Guest":
         user_access = frappe.db.get_value(
@@ -23,19 +23,19 @@ def user_has_permission(doc, ptype, user):
         )
         if user_access:
             if ptype == "share" and user_access["share"]:
-                return
+                return True
             if ptype == "write" and user_access["write"]:
-                return
+                return True
             if ptype == "read" and user_access["read"]:
-                return
+                return True
         group_access = user_group_entity_access(doc.name)
         if group_access:
             if ptype == "share" and group_access["read"]:
-                return
+                return True
             if ptype == "write" and group_access["write"]:
-                return
+                return True
             if ptype == "read" and group_access["read"]:
-                return
+                return True
         general_access = frappe.db.get_value(
             "Drive DocShare",
             {"share_name": doc.name, "everyone": 1},
@@ -44,9 +44,9 @@ def user_has_permission(doc, ptype, user):
         )
         if general_access:
             if ptype == "write" and general_access["write"]:
-                return
+                return True
             if ptype == "read" and general_access["read"]:
-                return
+                return True
         public_access = frappe.db.get_value(
             "Drive DocShare",
             {"share_name": doc.name, "public": 1},
@@ -55,9 +55,9 @@ def user_has_permission(doc, ptype, user):
         )
         if public_access:
             if ptype == "write" and public_access["write"]:
-                return
+                return True
             if ptype == "read" and public_access["read"]:
-                return
+                return True
 
         return False
     public_access = frappe.db.get_value(
@@ -68,6 +68,7 @@ def user_has_permission(doc, ptype, user):
     )
     if public_access:
         if ptype == "write" and public_access["write"]:
-            return
+            return True
         if ptype == "read" and public_access["read"]:
-            return
+            return True
+        return False
