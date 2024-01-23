3223c86a0d2b3943a784def6722cddc79cbf6297
Arjun Choudhary <contact@arjunchoudhary.com>
2024-01-23 10:17:27 +0000

    chore: posthog

--- drive/www/drive.py
+++ drive/www/drive.py
@@ -1,4 +1,6 @@
 import frappe
+from frappe.utils.telemetry import capture
+
 
 no_cache = 1
 
@@ -7,3 +9,6 @@ def get_context(context):
     csrf_token = frappe.sessions.get_csrf_token()
     frappe.db.commit()
     context.csrf_token = csrf_token
+    if frappe.session.user != 'Guest':
+        capture('active_site', 'drive')
+    return context
