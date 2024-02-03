cab9db8033c5afd22b85c2b357b5b3b0e62dd9da
Arjun Choudhary <contact@arjunchoudhary.com>
2024-02-03 08:09:02 +0000

    fix: incorrect message

--- frontend/src/pages/Trash.vue
+++ frontend/src/pages/Trash.vue
@@ -4,8 +4,8 @@
     :allowEmptyContextMenu="false"
     :isActive="false"
     :showSort="true"
-    :primary-message="`You haven't favourited any items`"
-    :secondary-message="'Items will appear here for easy access when you add them to favourites'" />
+    :primary-message="`Trash is currently empty`"
+    :secondary-message="'Items in the trash will be deleted automatically after 30 days'" />
 </template>
 
 <script setup>
