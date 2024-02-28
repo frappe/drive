ced72bbf7d56ed4cac8349648066e322e2bd5262
Arjun Choudhary <contact@arjunchoudhary.com>
2024-02-28 04:18:36 +0000

    switch from sql alter to

--- drive/install.py
+++ drive/install.py
@@ -2,7 +2,6 @@ import frappe
 
 
 def after_install():
-    frappe.db.sql("""ALTER TABLE `tabDrive Entity` MODIFY file_size BIGINT""")
     frappe.db.sql(
         """ALTER TABLE `tabDrive Entity` ADD FULLTEXT INDEX drive_entity_title_fts_idx (title)"""
     )
