3197912fba017d872feda92c1ddef907e60ccd6b
Arjun Choudhary <contact@arjunchoudhary.com>
2024-04-11 22:07:11 +0530

    feat: details component in editor

---
+++ frontend/src/components/DocEditor/DetailsExtension/index.ts
@@ -0,0 +1,3 @@
+export * from "./details";
+export * from "./details-item";
+export * from "./summary";
---
+++ frontend/src/components/DocEditor/utils/getNestedNodes.ts
@@ -0,0 +1,16 @@
+import { Attrs, Node as NodeElement } from "prosemirror-model";
+
+export function getNestedNodes(
+  node: NodeElement
+): Array<Array<Attrs | string>> {
+  const nodes: Array<Array<Attrs | string>> = [];
+
+  // @note: the content field is not array type
+  node.content.forEach((child) => {
+    if (child instanceof NodeElement) {
+      nodes.push([child.type.name, child.attrs]);
+    }
+  });
+
+  return nodes;
+}
