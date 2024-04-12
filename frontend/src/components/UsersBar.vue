104ec7938ba0f81110945c1413f71f473db11e18
Arjun Choudhary <contact@arjunchoudhary.com>
2024-04-12 23:24:54 +0530

    ui: realtime awareness userinfo

--- frontend/src/components/UsersBar.vue
+++ frontend/src/components/UsersBar.vue
@@ -1,33 +1,71 @@
 <template>
-  <button class="ml-2.5 flex justify-center items-center rounded-md">
-    <div
-      class="-ml-2.5 py-0.5 flex items-center rounded-full"
-      v-for="user in connectedUsers">
-      <Avatar
-        :size="'md'"
-        :style="{
-          border: '2px solid',
-          borderRadius: '100%',
-          borderColor: user.color,
-        }"
-        :label="user.name"
-        :title="user.name" />
-    </div>
-  </button>
+  <Popover
+    trigger="hover"
+    transition="default"
+    placement="bottom-end"
+    popoverClass="border-t-8 border-transparent">
+    <template #target="{ togglePopover }">
+      <div
+        class="ml-2.5 flex items-center rounded-md cursor-pointer"
+        @click="togglePopover()">
+        <div
+          class="-ml-2.5 -py-0.5 flex items-center rounded-full"
+          v-for="user in connectedUsers">
+          <Avatar
+            class="-"
+            :size="'md'"
+            :style="{
+              border: '2px solid',
+              borderRadius: '100%',
+              borderColor: user.color,
+            }"
+            :image="user.avatar"
+            :label="user.name"
+            :title="user.name" />
+        </div>
+      </div>
+    </template>
+    <template #body-main="{ togglePopover }">
+      <div class="px-3 py-1">
+        <div
+          class="flex justifty items-center gap-2 py-1"
+          v-for="user in connectedUsers">
+          <Avatar
+            :size="'md'"
+            :image="user.avatar"
+            :label="user.name"
+            :title="user.name" />
+          <span class="text-sm text-gray-800 mr-4">{{ user.name }}</span>
+          <svg
+            class="ml-auto"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <circle
+              cx="8"
+              cy="8"
+              r="4.5"
+              fill="transparent"
+              :stroke="user.color"
+              stroke-width="3" />
+          </svg>
+        </div>
+      </div>
+    </template>
+  </Popover>
 </template>
-
 <script>
-import { Avatar } from "frappe-ui";
+import { Avatar, Popover } from "frappe-ui";
+import { mapState } from "vuex";
 
 export default {
   name: "UsersBar",
   components: {
     Avatar,
+    Popover,
   },
-  computed: {
-    connectedUsers() {
-      return this.$store.state.connectedUsers;
-    },
-  },
+  computed: mapState(["connectedUsers"]),
 };
 </script>
