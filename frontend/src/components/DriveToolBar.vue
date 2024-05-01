7020e9a6999f35327b2f52fd9c10af88d8794aa5
Arjun Choudhary <contact@arjunchoudhary.com>
2024-05-01 15:35:16 +0530

    ui: update driveToolBar

--- frontend/src/components/DriveToolBar.vue
+++ frontend/src/components/DriveToolBar.vue
@@ -9,31 +9,19 @@
         class="mr-auto bg-gray-100 rounded-[10px] p-0.5 space-x-0.5 h-8 flex">
         <Button
           variant="ghost"
-          class="h-7 text-gray-900 rounded-[7px] px-2 py-1.5 leading-none transition-colors focus:outline-none"
+          class="leading-none transition-colors focus:outline-none"
           :class="[$store.state.shareView === 'with' ? 'bg-white shadow' : '']"
           @click="$store.commit('toggleShareView', 'with')">
           Shared with You
         </Button>
         <Button
           variant="ghost"
-          class="h-7 text-gray-900 rounded-[7px] px-2 py-1.5 leading-none transition-colors focus:outline-none"
+          class="leading-none transition-colors focus:outline-none"
           :class="[$store.state.shareView === 'by' ? 'bg-white shadow' : '']"
           @click="$store.commit('toggleShareView', 'by')">
           Shared by You
         </Button>
       </div>
-      <!-- <Dropdown
-        v-if="actionItems.length > 0"
-        :options="actionItems"
-        placement="left"
-        class="basis-5/12 lg:basis-auto">
-        <Button
-          class="text-sm h-8 w-full"
-          icon-right="chevron-down"
-          :loading="actionLoading">
-          <span class="hidden md:inline">Actions</span>
-        </Button>
-      </Dropdown> -->
       <Dropdown
         v-if="columnHeaders"
         :options="orderByItems"
@@ -43,24 +31,9 @@
           <Button
             class="text-sm h-8 px-2 border-r border-slate-200 rounded-r-none"
             @click.stop="toggleAscending">
-            <svg
-              class="h-4 w-4 stroke-current inline-block"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                :d="
-                  ascending
-                    ? 'M1.75 3.25h9m-9 4h6m-6 4h4m8.5-3.5l-2-2-2 2m2 4v-6'
-                    : 'M1.75 3.25h9m-9 4h6m-6 4h4m4.5-.5l2 2 2-2m-2 1v-6'
-                "
-                stroke-miterlimit="10"
-                stroke-linecap="round"
-                stroke-linejoin="round"></path>
-            </svg>
+            <ViewOrder :class="{ '[transform:rotateX(180deg)]': ascending }" />
           </Button>
           <Button class="text-sm h-8 rounded-l-none flex-1 hidden md:block">
-            Sort by {{ orderByLabel.toLowerCase() }}
-          </Button>
-          <Button class="text-sm h-8 rounded-l-none flex-1 md:hidden">
             {{ orderByLabel }}
           </Button>
         </div>
@@ -68,16 +41,18 @@
       <div class="bg-gray-100 rounded-md p-0.5 space-x-0.5 h-8 flex">
         <Button
           variant="ghost"
-          icon="grid"
-          class="h-7 text-gray-900 rounded-[7px] px-2 py-1.5 leading-none transition-colors focus:outline-none"
+          class="leading-none transition-colors focus:outline-none"
           :class="[$store.state.view === 'grid' ? 'bg-white shadow' : '']"
-          @click="$store.commit('toggleView', 'grid')"></Button>
+          @click="$store.commit('toggleView', 'grid')">
+          <ViewGrid />
+        </Button>
         <Button
           variant="ghost"
-          icon="list"
-          class="h-7 text-gray-900 rounded-[7px] px-2 py-1.5 leading-none transition-colors focus:outline-none"
+          class="leading-none transition-colors focus:outline-none"
           :class="[$store.state.view === 'list' ? 'bg-white shadow' : '']"
-          @click="$store.commit('toggleView', 'list')"></Button>
+          @click="$store.commit('toggleView', 'list')">
+          <ViewList />
+        </Button>
       </div>
     </div>
   </div>
@@ -85,7 +60,10 @@
 
 <script>
 import { Button, Dropdown } from "frappe-ui";
+import ViewGrid from "@/components/EspressoIcons/ViewGrid.vue";
+import ViewList from "@/components/EspressoIcons/ViewList.vue";
 import Breadcrumbs from "@/components/Breadcrumbs.vue";
+import ViewOrder from "./EspressoIcons/ViewOrder.vue";
 
 export default {
   name: "DriveToolBar",
@@ -93,6 +71,9 @@ export default {
     Button,
     Breadcrumbs,
     Dropdown,
+    ViewList,
+    ViewGrid,
+    ViewOrder,
   },
   props: {
     breadcrumbs: {
