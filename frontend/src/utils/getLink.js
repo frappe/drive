a8c88ac5894a0c91fba27bec29ed8c24eabeee61
Arjun Choudhary <contact@arjunchoudhary.com>
2024-05-01 20:21:17 +0530

    fix: getLink clipboard permission handling

--- frontend/src/utils/getLink.js
+++ frontend/src/utils/getLink.js
@@ -1,15 +1,32 @@
-import { toast } from "../utils/toasts.js";
+import { toast } from "../utils/toasts.js"
 
 export async function getLink(entity) {
   const link = entity.is_group
     ? `${window.location.origin}/drive/folder/${entity.name}`
     : entity.document
     ? `${window.location.origin}/drive/document/${entity.name}`
-    : `${window.location.origin}/drive/file/${entity.name}`;
-  await navigator.clipboard.writeText(link);
-  toast({
-    title: "Copied link",
-    position: "bottom-right",
-    timeout: 2,
-  });
+    : `${window.location.origin}/drive/file/${entity.name}`
+  navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
+    if (result.state == "denied") {
+      toast({
+        icon: "alert-triangle",
+        iconClasses: "text-red-700",
+        title: "Clipboard permission denied",
+        position: "bottom-right",
+      })
+    } else {
+      copyToClipboard(link)
+      toast({
+        title: "Copied link",
+        position: "bottom-right",
+        timeout: 2,
+      })
+    }
+  })
+}
+
+const copyToClipboard = (str) => {
+  if (navigator && navigator.clipboard && navigator.clipboard.writeText)
+    return navigator.clipboard.writeText(str)
+  return Promise.reject("The Clipboard API is not available.")
 }
