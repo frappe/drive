a5f27529e1997f36a8cd2a5db6d5185e40fa7197
Arjun Choudhary <contact@arjunchoudhary.com>
2024-05-24 15:54:59 +0530

    refactor: NewRoleDialog.vue

--- frontend/src/components/NewRoleDialog.vue
+++ frontend/src/components/NewRoleDialog.vue
@@ -1,20 +1,23 @@
 <template>
-  <Dialog v-model="open" :options="{ title: 'New Group Name', size: 'xs' }">
+  <Dialog v-model="open" :options="{ title: 'New Group', size: 'lg' }">
     <template #body-content>
-      <label class="block text-xs text-gray-600 mt-2 mb-1">Group Name</label>
-      <Input v-model="roleName" type="text" />
-      <label class="block text-xs text-gray-600 mt-2 mb-1">Add User</label>
+      <label class="block text-sm text-gray-600 mb-2">Group Name</label>
+      <Input v-model="roleName" placeholder="Group Name" type="text" />
+      <ErrorMessage class="text-sm mt-2" :message="groupNameError" />
+      <label class="block text-sm text-gray-600 mt-4 mb-1">Add User</label>
       <UserSearch
         :search-groups="false"
-        class="mb-4"
-        @submit="(user) => addUser(user)"
+        :active-users="uniqueUsers"
+        :active-groups="[]"
+        @add-new-users="(data) => addUser(data.users)"
       />
-      <label v-if="UsersInRole.length" class="block text-xs text-gray-600 mt-2">
+      <ErrorMessage class="text-sm mt-2" :message="memberError" />
+      <label v-if="UsersInRole.length" class="block text-sm text-gray-600 mt-6">
         Users in this Group
       </label>
       <div
-        v-for="user in uniqueUsers"
-        :key="user.email"
+        v-for="(user, index) in uniqueUsers"
+        :key="user.user_name"
         class="mt-1 flex flex-row w-full gap-2 items-center hover:bg-gray-50 rounded py-2 px-1 cursor-pointer group"
       >
         <Avatar :image="user.user_image" :label="user.full_name" size="xl" />
@@ -23,16 +26,26 @@
             {{ user.full_name }}
           </p>
           <p class="text-gray-600 text-sm">
-            {{ user.email }}
+            {{ user.user_name }}
           </p>
         </div>
-        <Button
-          class="ml-auto text-red-500 invisible group-hover:visible"
-          variant="minimal"
-          icon="trash-2"
-        />
+        <Dropdown
+          class="ml-auto"
+          :options="[
+            {
+              label: 'Remove',
+              icon: 'trash-2',
+              onClick: () => {
+                UsersInRole.splice(index, 1)
+              },
+            },
+          ]"
+          ><Button>
+            <template #icon>
+              <FeatherIcon name="more-horizontal" class="h-4 w-4" />
+            </template> </Button
+        ></Dropdown>
       </div>
-      <ErrorMessage class="mt-2" :message="errorMessage" />
       <div class="flex mt-6">
         <Button
           variant="solid"
@@ -46,12 +59,29 @@
   </Dialog>
 </template>
 <script>
-import { Avatar, Dialog, ErrorMessage, Input, Button } from "frappe-ui"
-import UserSearch from "./UserSearch.vue"
+import {
+  Avatar,
+  Dialog,
+  ErrorMessage,
+  Input,
+  Button,
+  Dropdown,
+  FeatherIcon,
+} from "frappe-ui"
+import UserSearch from "./ShareDialog/UserSearch.vue"
 
 export default {
   name: "RoleDetailsDialog",
-  components: { Avatar, Dialog, UserSearch, ErrorMessage, Button, Input },
+  components: {
+    Avatar,
+    Dialog,
+    UserSearch,
+    ErrorMessage,
+    Button,
+    Input,
+    Dropdown,
+    FeatherIcon,
+  },
   props: {
     modelValue: {
       type: Boolean,
@@ -63,16 +93,21 @@ export default {
     return {
       roleName: "",
       UsersInRole: [],
-      memberEmails: [],
+      groupNameError: [],
+      memberError: [],
       errorMessage: "",
     }
   },
   computed: {
-    uniqueUsers() {
-      return this.removeDuplicateObjects(this.UsersInRole, "email")
+    uniqueEmails() {
+      const uniqueUsernames = new Set()
+      this.UsersInRole.forEach((user) => {
+        uniqueUsernames.add(user.user_name)
+      })
+      return Array.from(uniqueUsernames)
     },
-    uniqueMemberEmails() {
-      return [...new Set(this.memberEmails)]
+    uniqueUsers() {
+      return this.removeDuplicateObjects(this.UsersInRole, "user_name")
     },
     open: {
       get() {
@@ -88,12 +123,15 @@ export default {
     },
   },
   methods: {
-    addUser(value) {
-      console.log(value)
-      this.UsersInRole.push(value)
-      this.memberEmails.push(value.email)
+    addUser(data) {
+      data.forEach((user) => {
+        this.memberError = ""
+        this.UsersInRole.push(user)
+      })
     },
     removeDuplicateObjects(arr, property) {
+      console.log(arr)
+      console.log(property)
       return [...new Map(arr.map((obj) => [obj[property], obj])).values()]
     },
   },
@@ -103,20 +141,27 @@ export default {
         url: "drive.utils.user_group.create_user_group",
         params: {
           group_name: this.roleName,
-          members: this.uniqueMemberEmails,
+          members: this.uniqueEmails,
         },
         validate: () => {
-          if (!this.memberEmails.length) {
-            this.errorMessage = "Group needs atleast one member"
+          if (!this.roleName.length) {
+            this.groupNameError = "Group name is required"
+          }
+          if (!this.uniqueEmails.length) {
+            this.memberError = "Group needs atleast one member"
           }
         },
         onSuccess(data) {
-          this.errorMessage = ""
+          if (data.error) {
+            this.groupNameError = data.message
+          }
           this.$emit("success", data)
         },
         onError(data) {
-          console.log(data)
-          this.errorMessage = data
+          console.log(data.messages)
+          if (data.messages === "Please set the document name") {
+            this.groupNameError = "Group name is required"
+          }
         },
         auto: false,
       }
