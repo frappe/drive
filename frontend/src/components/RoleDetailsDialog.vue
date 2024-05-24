1ecc5f4f3f9cce17089bf0484bf647032dee00ec
Arjun Choudhary <contact@arjunchoudhary.com>
2024-05-24 15:55:35 +0530

    refactor: RoleDetailsDialog.vue

--- frontend/src/components/RoleDetailsDialog.vue
+++ frontend/src/components/RoleDetailsDialog.vue
@@ -1,61 +1,79 @@
 <template>
-  <Dialog v-model="open" :options="{ title: roleName, size: 'xs' }">
+  <Dialog v-model="open" :options="{ title: roleName, size: 'lg' }">
     <template #body-content>
-      <label class="block text-xs text-gray-600 mt-2 mb-1">Add User</label>
+      <label class="block text-base text-gray-600 my-2">Add User</label>
       <UserSearch
+        :button-variant="'solid'"
         :search-groups="false"
-        class="mb-4"
-        @submit="(user) => addUser(user)"
+        :active-users="uniqueUsers"
+        :active-groups="[]"
+        @add-new-users="(data) => addUser(data.users)"
       />
-      <label v-if="UsersInRole.length" class="block text-xs text-gray-600 mt-2">
+      <label
+        v-if="UsersInRole.length"
+        class="block text-base text-gray-600 mt-6 mb-2"
+      >
         Users in this Group
       </label>
       <div
-        v-for="user in uniqueUsers"
-        :key="user.email"
-        class="mt-4 flex flex-row w-full gap-2 items-center hover:bg-gray-50 rounded py-2 px-1 cursor-pointer group"
+        v-for="(user, index) in uniqueUsers"
+        :key="user.user_name"
+        class="flex flex-row w-full gap-2 items-center rounded py-2 px-1"
       >
         <Avatar :image="user.user_image" :label="user.full_name" size="xl" />
         <div>
-          <p class="text-gray-900 text-sm font-medium">
+          <p class="text-gray-900 text-base font-medium">
             {{ user.full_name }}
           </p>
-          <p class="text-gray-600 text-sm">
-            {{ user.email }}
+          <p class="text-gray-600 text-base">
+            {{ user.user_name }}
           </p>
         </div>
-        <Button
-          class="ml-auto text-red-500 invisible group-hover:visible"
-          variant="minimal"
-          icon="trash-2"
-          @click="
-            $resources.RemoveUsersFromGroup.submit({
-              group_name: roleName,
-              user_emails: [user.email],
-            })
-          "
-        />
+        <Dropdown
+          class="ml-auto"
+          :options="[
+            {
+              label: 'Remove',
+              icon: 'trash-2',
+              onClick: () => {
+                removeUser(user)
+              },
+            },
+          ]"
+          ><Button>
+            <template #icon>
+              <FeatherIcon name="more-horizontal" class="h-4 w-4" />
+            </template> </Button
+        ></Dropdown>
       </div>
       <ErrorMessage class="mt-2" :message="errorMessage" />
-      <div class="flex mt-6">
-        <Button
-          variant="solid"
-          class="w-full"
-          @click="$resources.addUsersToGroup.submit()"
-        >
-          Done
-        </Button>
-      </div>
     </template>
   </Dialog>
 </template>
 <script>
-import { Avatar, Dialog, ErrorMessage, Button } from "frappe-ui"
-import UserSearch from "./UserSearch.vue"
+import {
+  Input,
+  Avatar,
+  Dialog,
+  ErrorMessage,
+  Button,
+  Dropdown,
+  FeatherIcon,
+} from "frappe-ui"
+import UserSearch from "./ShareDialog/UserSearch.vue"
 
 export default {
   name: "RoleDetailsDialog",
-  components: { Avatar, Dialog, UserSearch, ErrorMessage, Button },
+  components: {
+    Avatar,
+    Dialog,
+    UserSearch,
+    ErrorMessage,
+    Button,
+    Input,
+    Dropdown,
+    FeatherIcon,
+  },
   props: {
     modelValue: {
       type: Boolean,
@@ -70,17 +88,25 @@ export default {
   emits: ["update:modelValue", "success"],
   data() {
     return {
+      NewRoleName: "",
       UsersInRole: [],
-      memberEmails: [],
+      NewUsersInRole: [],
       errorMessage: "",
     }
   },
   computed: {
     uniqueUsers() {
-      return this.removeDuplicateObjects(this.UsersInRole, "email")
+      return this.removeDuplicateObjects(this.UsersInRole, "user_name")
     },
-    uniqueMemberEmails() {
-      return [...new Set(this.memberEmails)]
+    uniqueEmails() {
+      const uniqueUsernames = new Set()
+      this.NewUsersInRole.forEach((user) => {
+        uniqueUsernames.add(user)
+      })
+      this.UsersInRole.forEach((user) => {
+        uniqueUsernames.add(user.user_name)
+      })
+      return Array.from(uniqueUsernames)
     },
     open: {
       get() {
@@ -97,11 +123,20 @@ export default {
   },
   methods: {
     addUser(value) {
-      this.UsersInRole.push(value)
-      this.memberEmails.push(value.email)
+      value.forEach((user) => {
+        this.UsersInRole.push(user)
+        this.NewUsersInRole.push(user.user_name)
+      })
+      this.$resources.addUsersToGroup.submit()
     },
     removeDuplicateObjects(arr, property) {
       return [...new Map(arr.map((obj) => [obj[property], obj])).values()]
+    },
+    removeUser(user, index) {
+      this.$resources.RemoveUsersFromGroup.submit({
+        group_name: this.roleName,
+        user_emails: [user.user_name],
+      })
     },
   },
   resources: {
@@ -114,6 +149,12 @@ export default {
         onSuccess(data) {
           this.UsersInRole = data
           //this.uniqueUsers = data
+          console.log(data)
+          data.forEach((user) => {
+            user.user_name = user.email
+            user.user_type = "User"
+            delete user.name
+          })
         },
         onError(data) {
           console.log(data)
@@ -126,10 +167,10 @@ export default {
         url: "drive.utils.user_group.add_users_to_group",
         params: {
           group_name: this.roleName,
-          user_emails: this.uniqueMemberEmails,
+          user_emails: this.NewUsersInRole,
         },
         validate: () => {
-          if (!this.memberEmails.length) {
+          if (!this.uniqueEmails.length) {
             this.errorMessage = "Group needs atleast one member"
           }
         },
@@ -152,8 +193,8 @@ export default {
           user_emails: null,
         },
         validate: () => {
-          if (!this.memberEmails.length) {
-            this.errorMessage = "Group needs atleast one member"
+          if (this.uniqueEmails.length === 1) {
+            this.errorMessage = "Group needs atleast one active member"
           }
         },
         onSuccess() {
@@ -161,8 +202,9 @@ export default {
           this.$resources.getUsersInGroup.fetch()
         },
         onError(data) {
-          console.log(data)
-          this.errorMessage = data
+          if (data.messages === "Data missing in table: User Group Members'") {
+            this.errorMessage = "Group needs atleast one active member"
+          }
         },
         auto: false,
       }
