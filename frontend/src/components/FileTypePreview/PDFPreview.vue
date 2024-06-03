54c467b65de27f0f0b33dba3e09774fbf5978d07
Arjun Choudhary <contact@arjunchoudhary.com>
2024-06-03 20:30:40 +0530

    chore: move PDFPreview to PDFJS
    Regressions that'll need to be caught up
    - Scrolling large documents: This has a fallback to pagination to prevent oom crashes
      - Ideally the an intersectionObserver based scroll container is the fix
    - Table of Contents: Overview of all pages (usually on the left sidebar)
    - Search: Searching within the PDF isnt supported on large documents
      - Browser inbuilt search works for now for scrollable documents

--- frontend/src/components/FileTypePreview/PDFPreview.vue
+++ frontend/src/components/FileTypePreview/PDFPreview.vue
@@ -3,18 +3,65 @@
     v-if="loading"
     class="w-10 h-full text-neutral-100 mx-auto"
   />
-  <div
-    v-else
-    class="max-h-[95vh] max-w-[75vw] bg-[#252728] rounded-lg shadow-xl"
-  >
-    <iframe class="w-full min-w-[75vw] h-[90vh]" :src="previewURL" />
+  <div v-else class="flex flex-col items-start justify-start text-base">
+    <!-- Toolbar -->
+    <div
+      v-if="pages > 100"
+      class="flex w-full items-center justify-center rounded-t bg-gray-50 bg-opacity-50 mt-8 border gap-x-2 p-1"
+    >
+      <Button variant="ghost" icon="chevron-left">Prev</Button>
+      <Input
+        v-model.number="currentPage"
+        type="number"
+        class="min-8 w-auto max-w-15 text-center"
+        min="1"
+        max="pages"
+      />
+      <span> / {{ pages }}</span>
+      <Button
+        variant="ghost"
+        icon="chevron-right"
+        @click="
+          currentPage = currentPage < pages ? currentPage + 1 : currentPage
+        "
+      ></Button>
+    </div>
+
+    <div
+      class="bg-gray-400 overflow-auto h-[85vh] w-[55vw] p-2 border mt-2"
+      :class="pages > 100 ? 'rounded-b' : 'rounded'"
+    >
+      <div v-if="pages > 100" class="m-4">
+        <!-- Paginate -->
+        <VuePDF
+          id="pdf"
+          :pdf="pdf"
+          :page="currentPage"
+          :text-layer="true"
+          :fit-parent="true"
+        />
+      </div>
+      <!-- Scroll -->
+      <div v-for="page in pages" v-else :key="page" class="m-4">
+        <VuePDF
+          id="pdf"
+          :pdf="pdf"
+          :page="page"
+          :text-layer="true"
+          :fit-parent="true"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { LoadingIndicator } from "frappe-ui"
-import { onMounted, ref, watch } from "vue"
+import { onMounted, onUnmounted, ref, watch } from "vue"
 import { useObjectUrl } from "@vueuse/core"
+import { Button, Input } from "frappe-ui"
+import { VuePDF, usePDF } from "@tato30/vue-pdf"
+import "@tato30/vue-pdf/style.css"
 
 const props = defineProps({
   previewEntity: {
@@ -25,6 +72,8 @@ const props = defineProps({
 const loading = ref(true)
 const blob = ref(null)
 const previewURL = useObjectUrl(blob)
+const currentPage = ref(1)
+const { pdf, pages } = usePDF(previewURL)
 
 async function fetchContent() {
   loading.value = true
@@ -57,5 +106,25 @@ watch(
 onMounted(() => {
   fetchContent()
 })
+onUnmounted(() => {
+  pdf.value?.destroy()
+})
 </script>
-<style scoped></style>
+<style>
+#pdf {
+  user-select: text;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+}
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type="number"] {
+  appearance: inherit;
+  -moz-appearance: textfield;
+}
+</style>
