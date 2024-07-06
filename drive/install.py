db0f27ce631bd7231c12d741041674804c1cde42
Pete Argent <pete@peterargent.com>
2024-07-06 22:48:02 +1000

    Update install.py
    Changed from tabDrive to Drive due to error on install

--- drive/install.py
+++ drive/install.py
@@ -3,5 +3,5 @@ import frappe
 
 def after_install():
     frappe.db.sql(
-        """ALTER TABLE `tabDrive Entity` ADD FULLTEXT INDEX drive_entity_title_fts_idx (title)"""
+        """ALTER TABLE `Drive Entity` ADD FULLTEXT INDEX drive_entity_title_fts_idx (title)"""
     )
