f1fe2bd0a64004671d9ab9c67274eb1c8425dbca
Arjun Choudhary <contact@arjunchoudhary.com>
2024-08-02 11:04:10 +0530

    chore: remove explicit size in svg

--- frontend/src/components/EspressoIcons/File.vue
+++ frontend/src/components/EspressoIcons/File.vue
@@ -1,11 +1,5 @@
 <template>
-  <svg
-    width="1em"
-    height="1em"
-    viewBox="0 0 20 20"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
+  <svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
       d="M11.5 3V7.5H16M15.6213 6.12132L12.8787 3.37868C12.3161 2.81607 11.553 2.5 10.7574 2.5H5.5C4.39543 2.5 3.5 3.39543 3.5 4.5V15.5C3.5 16.6046 4.39543 17.5 5.5 17.5H14.5C15.6046 17.5 16.5 16.6046 16.5 15.5V8.24264C16.5 7.44699 16.1839 6.68393 15.6213 6.12132Z"
       stroke="currentColor"
