97ad15da37dcba7bf4608743dcc30b8d6fcd78c6
Arjun Choudhary <contact@arjunchoudhary.com>
2024-09-03 21:19:23 +0530

    ui: fix incorrect divider on foldersBefore

--- frontend/src/components/ListView.vue
+++ frontend/src/components/ListView.vue
@@ -51,102 +51,102 @@
       <div />
     </div>
     <div v-if="foldersBefore">
-      <div
-        v-for="entity in folders"
-        :id="entity.name"
-        :key="entity.name"
-        class="entity grid items-center cursor-pointer rounded px-2 py-1.5 hover:bg-gray-50 group"
-        :style="{
-          gridTemplateColumns: tableColumnsGridWidth,
-        }"
-        :class="
-          selectedEntities.includes(entity)
-            ? 'bg-gray-100'
-            : 'hover:bg-gray-100'
-        "
-        :draggable="false"
-        @[action]="dblClickEntity(entity)"
-        @click="selectEntity(entity, $event, folderContents)"
-        @contextmenu="handleEntityContext(entity, $event, folderContents)"
-        @dragstart="dragStart(entity, $event)"
-        @dragenter.prevent
-        @dragover.prevent
-        @mousedown.stop
-        @drop="isGroupOnDrop(entity)"
-      >
+      <div v-for="entity in folders" :key="entity.name">
         <div
-          class="flex items-center text-gray-800 text-base font-medium truncate"
+          :id="entity.name"
+          class="entity grid items-center cursor-pointer rounded px-2 py-1.5 hover:bg-gray-50 group"
+          :style="{
+            gridTemplateColumns: tableColumnsGridWidth,
+          }"
+          :class="
+            selectedEntities.includes(entity)
+              ? 'bg-gray-100'
+              : 'hover:bg-gray-100'
+          "
           :draggable="false"
+          @[action]="dblClickEntity(entity)"
+          @click="selectEntity(entity, $event, folderContents)"
+          @contextmenu="handleEntityContext(entity, $event, folderContents)"
+          @dragstart="dragStart(entity, $event)"
+          @dragenter.prevent
+          @dragover.prevent
+          @mousedown.stop
+          @drop="isGroupOnDrop(entity)"
         >
-          <svg
-            v-if="entity.is_group"
-            class="h-auto w-5 mr-3"
+          <div
+            class="flex items-center text-gray-800 text-base font-medium truncate"
             :draggable="false"
-            :style="{ fill: entity.color }"
-            width="16"
-            height="16"
-            viewBox="0 0 16 16"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
           >
-            <g clip-path="url(#clip0_1942_59507)">
-              <path
-                d="M7.83412 2.88462H1.5C1.22386 2.88462 1 3.10847 1 3.38462V12.5C1 13.6046 1.89543 14.5 3 14.5H13C14.1046 14.5 15 13.6046 15 12.5V2C15 1.72386 14.7761 1.5 14.5 1.5H9.94008C9.88623 1.5 9.83382 1.51739 9.79065 1.54957L8.13298 2.78547C8.04664 2.84984 7.94182 2.88462 7.83412 2.88462Z"
-              />
-            </g>
-            <defs>
-              <clipPath id="clip0_1942_59507">
-                <rect width="16" height="16" fill="white" />
-              </clipPath>
-            </defs>
-          </svg>
-          <img
-            v-else
-            :src="getIconUrl(formatMimeType(entity.mime_type))"
-            :draggable="false"
-            class="h-[20px] mr-3"
-          />
-          {{ entity.title }}
-        </div>
-        <div
-          class="hidden sm:flex items-center justify-start text-gray-700 text-base truncate"
-        >
-          <Avatar
-            :image="entity.user_image"
-            :label="entity.full_name"
-            class="-relative mr-2"
-            size="sm"
-          />
-          {{ entity.owner }}
-        </div>
-        <div
-          :title="entity.modified"
-          class="hidden sm:flex items-center justify-end text-gray-700 text-base truncate"
-        >
-          {{ entity.relativeModified }}
-        </div>
-        <div class="flex w-full justify-end text-base text-gray-700">
-          {{ entity.file_size }}
-        </div>
-        <div class="flex w-full justify-end">
-          <Button
-            :variant="'ghost'"
-            :model-value="selectedEntities.includes(entity)"
-            :class="
-              selectedEntities.includes(entity)
-                ? 'visible bg-gray-300'
-                : 'bg-inherit visible'
-            "
-            class="border-1 duration-300 relative ml-auto visible group-hover:visible"
-            @click.stop="
-              handleEntityContext(entity, $event, displayOrderedEntities)
-            "
+            <svg
+              v-if="entity.is_group"
+              class="h-auto w-5 mr-3"
+              :draggable="false"
+              :style="{ fill: entity.color }"
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g clip-path="url(#clip0_1942_59507)">
+                <path
+                  d="M7.83412 2.88462H1.5C1.22386 2.88462 1 3.10847 1 3.38462V12.5C1 13.6046 1.89543 14.5 3 14.5H13C14.1046 14.5 15 13.6046 15 12.5V2C15 1.72386 14.7761 1.5 14.5 1.5H9.94008C9.88623 1.5 9.83382 1.51739 9.79065 1.54957L8.13298 2.78547C8.04664 2.84984 7.94182 2.88462 7.83412 2.88462Z"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0_1942_59507">
+                  <rect width="16" height="16" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+            <img
+              v-else
+              :src="getIconUrl(formatMimeType(entity.mime_type))"
+              :draggable="false"
+              class="h-[20px] mr-3"
+            />
+            {{ entity.title }}
+          </div>
+          <div
+            class="hidden sm:flex items-center justify-start text-gray-700 text-base truncate"
           >
-            <FeatherIcon class="h-4" name="more-horizontal" />
-          </Button>
+            <Avatar
+              :image="entity.user_image"
+              :label="entity.full_name"
+              class="-relative mr-2"
+              size="sm"
+            />
+            {{ entity.owner }}
+          </div>
+          <div
+            :title="entity.modified"
+            class="hidden sm:flex items-center justify-end text-gray-700 text-base truncate"
+          >
+            {{ entity.relativeModified }}
+          </div>
+          <div class="flex w-full justify-end text-base text-gray-700">
+            {{ entity.file_size }}
+          </div>
+          <div class="flex w-full justify-end">
+            <Button
+              :variant="'ghost'"
+              :model-value="selectedEntities.includes(entity)"
+              :class="
+                selectedEntities.includes(entity)
+                  ? 'visible bg-gray-300'
+                  : 'bg-inherit visible'
+              "
+              class="border-1 duration-300 relative ml-auto visible group-hover:visible"
+              @click.stop="
+                handleEntityContext(entity, $event, displayOrderedEntities)
+              "
+            >
+              <FeatherIcon class="h-4" name="more-horizontal" />
+            </Button>
+          </div>
         </div>
+        <div class="mx-2 h-px border-t border-gray-200"></div>
       </div>
-      <div class="mx-2 h-px border-t border-gray-200"></div>
     </div>
     <div v-for="entity in files" :key="entity.name">
       <div
