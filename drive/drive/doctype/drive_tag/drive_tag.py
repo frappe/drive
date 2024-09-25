f0c2a5998d6be9ecda7eeb059431368d9641cd13
Arjun Choudhary <contact@arjunchoudhary.com>
2024-09-25 09:47:53 +0530

    chore: remove unused hook

--- drive/drive/doctype/drive_tag/drive_tag.py
+++ drive/drive/doctype/drive_tag/drive_tag.py
@@ -3,14 +3,7 @@
 
 import frappe
 from frappe.model.document import Document
-from drive.api.tags import remove_tag
 
 
 class DriveTag(Document):
-    def on_trash(self):
-        drive_entities = frappe.db.get_list(
-            "Drive Entity", filters={"owner": self.owner}, pluck="name"
-        )
-
-        for entity in drive_entities:
-            remove_tag(entity, self.name)
+    pass
