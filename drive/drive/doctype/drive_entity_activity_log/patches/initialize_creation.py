91dacb69c224a117395f5f4efcdf2fcd7a663291
Arjun Choudhary <contact@arjunchoudhary.com>
2024-10-10 14:55:55 +0530

    refactor(db): Activity log creation patch

--- drive/drive/doctype/drive_entity_activity_log/patches/initialize_creation.py
+++ drive/drive/doctype/drive_entity_activity_log/patches/initialize_creation.py
@@ -1,18 +1,16 @@
 import frappe
 
 
-all_entities = frappe.db.get_list("Drive Entity", fields=["name", "title", "owner", "creation"])
-
-for i in all_entities:
-    doc = frappe.get_doc(
-        {
-            "doctype": "Drive Entity Activity Log",
-            "entity": i.name,
-            "action_type": "create",
-            "message": f"Created {i.title}",
-            "owner": i.owner,
-        }
+def execute():
+    all_entities = frappe.db.get_list(
+        "Drive Entity", fields=["name", "title", "owner", "creation"]
     )
-    doc.insert()
-    frappe.db.set_value("Drive Entity Activity Log", doc.name, "creation", i.creation)
 
+    for i in all_entities:
+        doc = frappe.new_doc("Drive Entity Activity Log")
+        doc.entity = i.name
+        doc.action_type = "create"
+        doc.message = f"Created {i.title}"
+        doc.save()
+        frappe.db.set_value("Drive Entity Activity Log", doc.name, "owner", i.owner)
+        frappe.db.set_value("Drive Entity Activity Log", doc.name, "creation", i.creation)
