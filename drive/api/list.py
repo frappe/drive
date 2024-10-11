fc337975f7d0c82ca840342dc92a0ea587b7732a
Arjun Choudhary <contact@arjunchoudhary.com>
2024-10-11 18:55:40 +0530

    feat(api): Drive Guest shared list

--- drive/api/list.py
+++ drive/api/list.py
@@ -404,3 +404,227 @@ def shared_by_user(
         query = query.where((Criterion.any(mime_type_criterion)) | (DriveEntity.is_group == True))
 
     return query.run(as_dict=True)
+
+
+@frappe.whitelist()
+def shared_with_guest(
+    order_by="modified",
+    limit=100,
+    offset=0,
+    folders_first=True,
+    file_kind_list=[],
+    tag_list=[],
+    mime_type_list=[],
+):
+    """
+    Returns the highest level of shared items shared with/by the current user, group or org
+
+    :param entity_name: Document-name of the folder whose contents are to be listed.
+    :raises NotADirectoryError: If this DriveEntity doc is not a folder
+    :return: List of DriveEntities with permissions
+    :rtype: list[frappe._dict]
+    """
+    if isinstance(folders_first, str):
+        try:
+            folders_first = json.loads(folders_first)
+        except json.JSONDecodeError:
+            frappe.log_error(
+                "Invalid JSON for folders_first: {}".format(folders_first), "JSONDecodeError"
+            )
+            folders_first = True
+    folders_first = bool(folders_first)
+    DriveDocShare = frappe.qb.DocType("Drive DocShare")
+    DriveEntity = frappe.qb.DocType("Drive Entity")
+    DriveFavourite = frappe.qb.DocType("Drive Favourite")
+    UserGroupMember = frappe.qb.DocType("User Group Member")
+    DriveUser = frappe.qb.DocType("User")
+    selectedFields = [
+        DriveEntity.name,
+        DriveEntity.title,
+        DriveEntity.is_group,
+        DriveEntity.owner,
+        DriveUser.full_name,
+        DriveUser.user_image,
+        DriveEntity.modified,
+        DriveEntity.creation,
+        DriveEntity.file_size,
+        DriveEntity.file_kind,
+        DriveEntity.file_ext,
+        DriveEntity.color,
+        DriveEntity.document,
+        DriveEntity.mime_type,
+        DriveEntity.parent_drive_entity,
+        DriveEntity.allow_download,
+        DriveEntity.is_active,
+        DriveEntity.allow_comments,
+        DriveDocShare.user_name,
+        DriveDocShare.user_doctype,
+        DriveDocShare.read,
+        DriveDocShare.write,
+        DriveDocShare.everyone,
+        DriveDocShare.share,
+        DriveDocShare.share_parent,
+        DriveFavourite.entity.as_("is_favourite"),
+    ]
+
+    query = (
+        frappe.qb.from_(DriveEntity)
+        .inner_join(DriveDocShare)
+        .on((DriveDocShare.share_name == DriveEntity.name))
+        .left_join(UserGroupMember)
+        .on((UserGroupMember.parent == DriveDocShare.user_name))
+        .left_join(DriveFavourite)
+        .on(
+            (DriveFavourite.entity == DriveEntity.name)
+            & (DriveFavourite.user == frappe.session.user)
+        )
+        .left_join(DriveUser)
+        .on((DriveEntity.owner == DriveUser.email))
+        .offset(offset)
+        .limit(limit)
+        .select(*selectedFields)
+        .where(DriveEntity.is_active == 1)
+        .where(
+            (UserGroupMember.user == frappe.session.user)
+            | (DriveDocShare.user_name == frappe.session.user)
+        )
+        .where(DriveDocShare.share_parent.isnull())
+        .where(DriveEntity.owner != frappe.session.user)
+        .groupby(DriveEntity.name)
+    )
+    if folders_first:
+        query = query.orderby(
+            Case().when(DriveEntity.is_group == True, 1).else_(2),
+            Order.desc,
+        )
+
+    query = query.orderby(
+        order_by.split()[0],
+        order=Order.desc if order_by.endswith("desc") else Order.asc,
+    )
+    if file_kind_list:
+        file_kind_list = json.loads(file_kind_list)
+        file_kind_criterion = [DriveEntity.file_kind == file_kind for file_kind in file_kind_list]
+        query = query.where(Criterion.any(file_kind_criterion))
+
+    if tag_list:
+        tag_list = json.loads(tag_list)
+        query = query.left_join(DriveEntityTag).on(DriveEntityTag.parent == DriveEntity.name)
+        tag_list_criterion = [DriveEntityTag.tag == tags for tags in tag_list]
+        query = query.where(Criterion.any(tag_list_criterion))
+
+    if mime_type_list:
+        mime_type_list = json.loads(mime_type_list)
+        mime_type_criterion = [DriveEntity.mime_type == mime_type for mime_type in mime_type_list]
+        query = query.where((Criterion.any(mime_type_criterion)) | (DriveEntity.is_group == True))
+
+    return query.run(as_dict=True)
+
+
+@frappe.whitelist()
+def shared_by_guest(
+    order_by="modified",
+    limit=100,
+    offset=0,
+    folders_first=True,
+    file_kind_list=[],
+    tag_list=[],
+    mime_type_list=[],
+):
+    """
+    Return the list of files and folders shared with the current user
+
+    :param entity_name: Document-name of the folder whose contents are to be listed.
+    :raises NotADirectoryError: If this DriveEntity doc is not a folder
+    :return: List of DriveEntities with permissions
+    :rtype: list[frappe._dict]
+    """
+    if isinstance(folders_first, str):
+        try:
+            folders_first = json.loads(folders_first)
+        except json.JSONDecodeError:
+            frappe.log_error(
+                "Invalid JSON for folders_first: {}".format(folders_first), "JSONDecodeError"
+            )
+            folders_first = True
+    folders_first = bool(folders_first)
+    DriveDocShare = frappe.qb.DocType("Drive DocShare")
+    DriveEntity = frappe.qb.DocType("Drive Entity")
+    DriveFavourite = frappe.qb.DocType("Drive Favourite")
+    DriveUser = frappe.qb.DocType("User")
+    selectedFields = [
+        DriveEntity.name,
+        DriveEntity.title,
+        DriveEntity.is_group,
+        DriveEntity.owner,
+        DriveUser.full_name,
+        DriveUser.user_image,
+        DriveEntity.modified,
+        DriveEntity.creation,
+        DriveEntity.file_size,
+        DriveEntity.file_kind,
+        DriveEntity.file_ext,
+        DriveEntity.color,
+        DriveEntity.document,
+        DriveEntity.mime_type,
+        DriveEntity.parent_drive_entity,
+        DriveEntity.allow_download,
+        DriveEntity.is_active,
+        DriveEntity.allow_comments,
+        DriveDocShare.read,
+        DriveDocShare.write,
+        DriveDocShare.everyone,
+        DriveDocShare.share,
+        DriveDocShare.owner_parent,
+        DriveFavourite.entity.as_("is_favourite"),
+    ]
+
+    query = (
+        frappe.qb.from_(DriveEntity)
+        .left_join(DriveDocShare)
+        .on(
+            (DriveDocShare.share_name == DriveEntity.name)
+            & (DriveEntity.owner == frappe.session.user)
+        )
+        .left_join(DriveUser)
+        .on((DriveEntity.owner == DriveUser.email))
+        .left_join(DriveFavourite)
+        .on(
+            (DriveFavourite.entity == DriveEntity.name)
+            & (DriveFavourite.user == frappe.session.user)
+        )
+        .offset(offset)
+        .limit(limit)
+        .select(*selectedFields)
+        .where(DriveEntity.is_active == 1)
+        .where((DriveDocShare.user_name != frappe.session.user))
+        .where(DriveDocShare.owner_parent.isnull())
+        .groupby(DriveEntity.name)
+    )
+    if folders_first:
+        query = query.orderby(
+            Case().when(DriveEntity.is_group == True, 1).else_(2),
+            Order.desc,
+        )
+
+    query = query.orderby(
+        order_by.split()[0],
+        order=Order.desc if order_by.endswith("desc") else Order.asc,
+    )
+    if file_kind_list:
+        file_kind_list = json.loads(file_kind_list)
+        file_kind_criterion = [DriveEntity.file_kind == file_kind for file_kind in file_kind_list]
+        query = query.where(Criterion.any(file_kind_criterion))
+
+    if tag_list:
+        tag_list = json.loads(tag_list)
+        query = query.left_join(DriveEntityTag).on(DriveEntityTag.parent == DriveEntity.name)
+        tag_list_criterion = [DriveEntityTag.tag == tags for tags in tag_list]
+        query = query.where(Criterion.any(tag_list_criterion))
+
+    if mime_type_list:
+        mime_type_list = json.loads(mime_type_list)
+        mime_type_criterion = [DriveEntity.mime_type == mime_type for mime_type in mime_type_list]
+        query = query.where((Criterion.any(mime_type_criterion)) | (DriveEntity.is_group == True))
+
+    return query.run(as_dict=True)
