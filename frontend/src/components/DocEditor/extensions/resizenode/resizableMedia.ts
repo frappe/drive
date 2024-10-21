05d5b199def7a52259733bbb3e3fc39c55e7aec0
Arjun Choudhary <contact@arjunchoudhary.com>
2024-10-21 12:19:15 +0530

    chore: default to null for new rsz node

--- frontend/src/components/DocEditor/extensions/resizenode/resizableMedia.ts
+++ frontend/src/components/DocEditor/extensions/resizenode/resizableMedia.ts
@@ -72,7 +72,7 @@ export const ResizableMedia = Node.create<MediaOptions>({
         default: "auto",
       },
       dataAlign: {
-        default: "center", // 'left' | 'center' | 'right'
+        default: null, // 'left' | 'center' | 'right'
       },
       dataFloat: {
         default: null, // 'left' | 'right'
