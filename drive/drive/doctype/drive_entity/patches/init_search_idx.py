e60caf7f57f45981de1f3a6105e2de3e97f78000
Arjun Choudhary <contact@arjunchoudhary.com>
2024-10-30 18:22:04 +0530

    fix: patch mariaDB fts index

---
+++ drive/drive/doctype/drive_entity/patches/init_search_idx.py
@@ -0,0 +1,7 @@
+import frappe
+
+def execute():
+    index_check = frappe.db.sql("""SHOW INDEX FROM `tabDrive Entity` WHERE Key_name = 'drive_entity_title_fts_idx'""")
+
+    if not index_check:
+        frappe.db.sql("""ALTER TABLE `tabDrive Entity` ADD FULLTEXT INDEX drive_entity_title_fts_idx (title)""")
