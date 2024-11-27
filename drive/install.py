de001f2183599b8a991a1e283321edd19ec603c8
Arjun Choudhary <contact@arjunchoudhary.com>
2024-11-27 08:55:48 +0530

    chore: skip idx if already exists

--- drive/install.py
+++ drive/install.py
@@ -1,7 +1,10 @@
 import frappe
 
-
 def after_install():
-    frappe.db.sql(
-        """ALTER TABLE `tabDrive Entity` ADD FULLTEXT INDEX drive_entity_title_fts_idx (title)"""
+    index_check = frappe.db.sql(
+        """SHOW INDEX FROM `tabDrive Entity` WHERE Key_name = 'drive_entity_title_fts_idx'"""
     )
+    if not index_check:
+        frappe.db.sql(
+            """ALTER TABLE `tabDrive Entity` ADD FULLTEXT INDEX drive_entity_title_fts_idx (title)"""
+        )
