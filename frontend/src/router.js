769f5a5146fdfa157bddcb6f03a9bb396fc48c66
Safwan Samsudeen <safwansamsudeen.c@gmail.com>
2025-05-29 12:22:24 +0530

    chore: import

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router"
 import store from "./store"
 import { manageBreadcrumbs } from "./utils/files"
+import { createResource } from "frappe-ui"
 
 function clearStore() {
   store.commit("setActiveEntity", null)
