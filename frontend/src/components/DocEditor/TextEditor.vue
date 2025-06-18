793a88704480876f023e2b17b533a40c5e5c5d02
Safwan Samsudeen <safwansamsudeen.c@gmail.com>
2025-06-18 18:36:49 +0530

    feat: adding and editing comments

--- frontend/src/components/DocEditor/TextEditor.vue
+++ frontend/src/components/DocEditor/TextEditor.vue
@@ -141,39 +141,26 @@ import H1 from "./icons/h-1.vue"
 import H2 from "./icons/h-2.vue"
 import H3 from "./icons/h-3.vue"
 import { LucideMessageCircle } from "lucide-vue-next"
+import store from "../../store"
 
 const autosave = debounce(() => emit("saveDocument"), 1000)
 const comments = ref([
   {
     name: "3fdac807-7509-4fe8-b830-71422e1e2eb6",
     owner: "safwan@frappe.io",
-    content: `fly to, your city, excited, to see your face. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor aliquam nulla facilisi cras fermentum odio eu feugiat pretium. Leo a diam sollicitudin tempor id eu nisl. 
-
-- Cum sociis natoque penatibus et. Pulvinar pellentesque habitant morbi tristique senectus et netus et. 
-- Aliquet eget sit amet tellus cras adipiscing enim. Enim nunc faucibus a pellentesque sit amet porttitor eget dolor. 
-- Fermentum posuere urna nec tincidunt praesent semper feugiat nibh sed. Euismod elementum nisi quis eleifend quam. 
-- Vitae purus faucibus ornare suspendisse sed nisi lacus sed.
-
-Iaculis urna id volutpat lacus laoreet non. Massa tincidunt dui ut ornare lectus sit amet est placerat. Nec sagittis aliquam malesuada bibendum arcu vitae. Vel risus commodo viverra maecenas accumsan lacus vel. Libero justo laoreet sit amet cursus sit amet dictum. Integer vitae justo eget magna fermentum. Sapien pellentesque habitant morbi tristique senectus et. Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Molestie ac feugiat sed lectus vestibulum mattis. Quis commodo odio aenean sed adipiscing diam donec adipiscing tristique. Cursus eget nunc scelerisque viverra mauris. Nunc congue nisi vitae suscipit tellus mauris a diam. Dui nunc mattis enim ut tellus.
-
-At in tellus integer feugiat scelerisque. Cursus risus at ultrices mi tempus imperdiet nulla malesuada pellentesque. Adipiscing elit pellentesque habitant morbi tristique. Cras fermentum odio eu feugiat pretium. In massa tempor nec feugiat nisl. Amet luctus venenatis lectus magna fringilla urna porttitor. Sed faucibus turpis in eu mi bibendum neque egestas. Diam maecenas sed enim ut. Quis blandit turpis cursus in hac habitasse platea dictumst quisque. Volutpat est velit egestas dui. Lacus laoreet non curabitur gravida arcu ac tortor dignissim convallis. Placerat vestibulum lectus mauris ultrices eros in cursus turpis massa. Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus. Ultricies tristique nulla aliquet enim tortor at auctor urna. Nunc mattis enim ut tellus elementum sagittis vitae.
-
-Scelerisque varius morbi enim nunc faucibus a pellentesque sit amet. Feugiat nibh sed pulvinar proin gravida hendrerit lectus a. Quis ipsum suspendisse ultrices gravida dictum fusce. Ultrices tincidunt arcu non sodales neque. Malesuada bibendum arcu vitae elementum curabitur vitae nunc. Enim nec dui nunc mattis enim ut tellus. Id eu nisl nunc mi ipsum faucibus. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum. Risus pretium quam vulputate dignissim suspendisse in est ante. Amet consectetur adipiscing elit pellentesque. Purus faucibus ornare suspendisse sed.
-
-Aliquam nulla facilisi cras fermentum odio eu. Venenatis urna cursus eget nunc scelerisque viverra. Arcu non odio euismod lacinia at quis risus. Semper risus in hendrerit gravida rutrum. Nisi est sit amet facilisis magna. Proin nibh nisl condimentum id venenatis a. Ut aliquam purus sit amet luctus venenatis lectus. Morbi blandit cursus risus at ultrices mi tempus imperdiet nulla. Purus in massa tempor nec feugiat. Bibendum ut tristique et egestas quis. Dui accumsan sit amet nulla facilisi morbi tempus iaculis urna. Risus in hendrerit gravida rutrum. Faucibus scelerisque eleifend donec pretium. Non sodales neque sodales ut etiam sit amet nisl. Quis enim lobortis scelerisque fermentum dui faucibus. Ut diam quam nulla porttitor massa id. Varius sit amet mattis vulputate enim. Vehicula ipsum a arcu cursus vitae congue. Lectus quam id leo in vitae turpis. Commodo elit at imperdiet dui accumsan sit.`,
-    created_on: "2:01 PM",
+    content: `fly to, your city, excited, to see your face.`,
+    created_on: new Date(),
     replies: [],
   },
   {
     name: "3c566683-6546-466e-8268-52199bc437c1",
     owner: "four@frappe.io",
     content: "give me your tired, huddled, masses; yearning to breathe free",
-    created_on: "2:10 PM",
+    created_on: new Date(),
     replies: [],
   },
 ])
+
 const props = defineProps({
   settings: Object,
   entity: Object,
@@ -212,7 +199,15 @@ const editorExtensions = [
       class: "",
     },
     onCommentActivated: (id) => {
-      if (id) setTimeout(() => (activeComment.value = id), 100)
+      if (id) {
+        setTimeout(() => (activeComment.value = id), 100)
+        let el = document.querySelector("#comment-" + id)
+        el.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+          inline: "nearest",
+        })
+      }
     },
   }),
 ]
@@ -388,7 +383,19 @@ const bubbleMenuButtons = [
   {
     label: "Comment",
     icon: LucideMessageCircle,
-    action: (editor) => editor.chain().focus().setComment(uuidv4()).run(),
+    action: (editor) => {
+      const id = uuidv4()
+      editor.chain().focus().setComment(id).run()
+      comments.value.push({
+        name: id,
+        owner: store.state.user.id,
+        created_on: new Date(),
+        content: "",
+        edit: true,
+        replies: [],
+      })
+      activeComment.value = id
+    },
     isActive: () => false,
   },
   "Image",
--- frontend/src/components/DocEditor/components/FloatingComments.vue
+++ frontend/src/components/DocEditor/components/FloatingComments.vue
@@ -1,6 +1,5 @@
 <template>
   <div
-    ref="comments-section"
     class="border-s-2 py-4 px-12 flex flex-col gap-8 justify-start max-h-full overflow-auto"
   >
     <div
@@ -34,22 +33,62 @@
           Delete
         </Button>
       </div>
-      <div class="px-3 py-2.5 flex gap-2 text-ink-gray-9">
-        <Avatar
-          :label="comment.owner"
-          :image="comment.image"
-        />
-        <div>
-          <div class="label-group flex gap-1 text-sm items-start">
+      <div class="px-3 py-2.5 text-ink-gray-9">
+        <div class="flex gap-2 items-center">
+          <Avatar
+            :label="comment.owner"
+            :image="comment.image"
+          />
+          <div class="label-group flex flex-col gap-1 text-sm items-start">
             <label class="font-medium text-ink-gray-8">{{
               comment.full_name
             }}</label>
-            <span class="text-ink-gray-7">{{ " ∙ " }}</span>
-            <label class="text-ink-gray-6">{{ comment.created_on }}</label>
-          </div>
-          <div class="comment-content text-sm mt-1">
-            {{ comment.content }}
 
+            <label class="text-ink-gray-6">{{
+              formatDate(comment.created_on)
+            }}</label>
+          </div>
+        </div>
+        <div class="comment-content text-sm my-2">
+          <TextEditor
+            v-if="comment.edit"
+            class="border rounded"
+            :content="comment.content"
+            :mentions="allUsers.data"
+            editor-class="text-sm p-2"
+            placeholder="Reply"
+            @change="(val) => (commentContents[comment.name] = val)"
+            :bubble-menu="[
+              'Bold',
+              'Italic',
+              'Strikethrough',
+              'Separator',
+              'Code',
+              'Blockquote',
+              'Separator',
+              ['Bullet List', 'Numbered List'],
+            ]"
+          >
+            <template #bottom>
+              <div class="flex justify-end mb-0.5 mr-0.5">
+                <Button
+                  variant="ghost"
+                  @click="
+                    () => {
+                      comment.content = commentContents[comment.name]
+                      comment.edit = false
+                    }
+                  "
+                >
+                  <template #icon>
+                    <LucideCheck class="size-3.5" />
+                  </template>
+                </Button>
+              </div>
+            </template>
+          </TextEditor>
+          <template v-else>
+            <div v-html="comment.content" />
             <div
               v-show="activeComment === comment.name"
               class="flex gap-1 mt-2"
@@ -58,45 +97,82 @@
                 variant="subtle"
                 size="xs"
                 class="font-medium"
+                @click="comment.edit = true"
               >
                 Edit
               </Button>
-              <Button
-                variant="subtle"
-                size="xs"
-                class="font-medium"
-                >Delete</Button
-              >
             </div>
-          </div>
+          </template>
         </div>
       </div>
       <div class="pb-3 px-3">
         <div
           v-if="activeComment === comment.name"
           v-for="reply in comment.replies"
-          class="flex gap py-2"
+          class="py-2"
           :key="reply.name"
         >
-          <Avatar
-            :label="reply.owner"
-            :image="reply.image"
-          />
-          <div class="px-2 group">
-            <div class="label-group flex gap-1 text-sm items-start">
+          <div class="flex gap-2 items-center">
+            <Avatar
+              :label="reply.owner"
+              :image="reply.image"
+            />
+            <div class="label-group flex flex-col gap-1 text-sm items-start">
               <label class="font-medium text-ink-gray-8">{{
                 reply.full_name
               }}</label>
-              <span class="text-ink-gray-7">{{ " ∙ " }}</span>
-              <label class="text-ink-gray-6">{{ comment.created_on }}</label>
+
+              <label class="text-ink-gray-6">{{
+                formatDate(reply.created_on)
+              }}</label>
             </div>
-            <div class="comment-content text-sm mt-1">
+          </div>
+          <div class="comment-content text-sm mt-2">
+            <TextEditor
+              v-if="reply.edit"
+              class="border rounded"
+              :content="reply.content"
+              :mentions="allUsers.data"
+              editor-class="text-sm p-2"
+              placeholder="Reply"
+              @change="(val) => (commentContents[reply.name] = val)"
+              :bubble-menu="[
+                'Bold',
+                'Italic',
+                'Strikethrough',
+                'Separator',
+                'Code',
+                'Blockquote',
+                'Separator',
+                ['Bullet List', 'Numbered List'],
+              ]"
+            >
+              <template #bottom>
+                <div class="flex justify-end mb-0.5 mr-0.5">
+                  <Button
+                    variant="ghost"
+                    @click="
+                      () => {
+                        reply.content = commentContents[reply.name]
+                        reply.edit = false
+                      }
+                    "
+                  >
+                    <template #icon>
+                      <LucideCheck class="size-3.5" />
+                    </template>
+                  </Button>
+                </div>
+              </template>
+            </TextEditor>
+            <template v-else>
               <div v-html="reply.content" />
               <div class="gap-1 mt-2 hidden group-hover:flex">
                 <Button
                   variant="subtle"
                   size="xs"
                   class="font-medium"
+                  @click="reply.edit = true"
                 >
                   Edit
                 </Button>
@@ -107,7 +183,7 @@
                   >Delete</Button
                 >
               </div>
-            </div>
+            </template>
           </div>
         </div>
         <div
@@ -118,8 +194,8 @@
           {{ comment.replies.length === 1 ? "reply" : "replies" }}
         </div>
         <TextEditor
-          v-if="activeComment === comment.name"
-          class="border rounded"
+          v-if="activeComment === comment.name && !comment.edit"
+          class="border rounded my-2"
           :mentions="allUsers.data"
           editor-class="text-sm p-2"
           placeholder="Reply"
@@ -156,20 +232,10 @@
 import { useTemplateRef, computed, reactive, watch } from "vue"
 import { allUsers } from "@/resources/permissions"
 import { Avatar, Button, TextEditor } from "frappe-ui"
-const commentsSection = useTemplateRef("comments-section")
+import { formatDate } from "@/utils/format"
 
 const activeComment = defineModel("activeComment")
 const comments = defineModel("comments")
-watch(activeComment, (val) => {
-  if (val) {
-    let el = commentsSection.value.querySelector("#comment-" + val)
-    el.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-      inline: "nearest",
-    })
-  }
-})
 
 const formattedComments = computed(() => {
   return comments.value.map((comment) => {
@@ -181,12 +247,13 @@ const formattedComments = computed(() => {
 })
 
 const newReplies = reactive({})
+const commentContents = reactive({})
 
 const newReply = (comment) => {
   comment.replies.push({
     content: newReplies[comment.name],
     owner: comment.owner,
-    created_on: "Now",
+    created_on: new Date(),
   })
   activeComment.value = ""
 }
