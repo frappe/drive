24e364ba6615ca8c531e337467e190679beee452
Safwan Samsudeen <safwansamsudeen.c@gmail.com>
2025-07-27 18:28:25 +0530

    fix: #415

--- drive/api/notifications.py
+++ drive/api/notifications.py
@@ -6,7 +6,7 @@ from pypika import Order
 def get_link(entity):
     type_ = {True: "file", bool(entity.is_group): "folder", bool(entity.document): "document"}
     return (
-        entity.path if entity.is_link else f"/drive/{entity.team}/{type_.get(True)}/{entity.name}/"
+        entity.path if entity.is_link else f"/drive/t/{entity.team}/{type_.get(True)}/{entity.name}/"
     )
 
 
