f85c9dc106cd755ad17bc1a3bc6f17c3708c3ec2
Devansh Khetan <56186839+Devanshk9@users.noreply.github.com>
2025-10-27 16:39:26 +0530

    fix: null on print

--- frontend/src/utils/files.js
+++ frontend/src/utils/files.js
@@ -487,7 +487,7 @@ export function printDoc(html, watermarkStatus) {
                  ${
                    watermarkStatus
                      ? `<div class="watermark">${watermarkText}</div>`
-                     : null
+                     : ""
                  }
                 <div class="ProseMirror prose-sm" style='padding-left: 40px; padding-right: 40px; padding-top: 20px; padding-bottom: 20px; margin: 0;'>
                   ${html}
