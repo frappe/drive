d0dc03410660efb75e3abd9526b8ce6ac738cac3
Safwan Samsudeen <safwansamsudeen.c@gmail.com>
2025-12-03 10:14:51 +0530

    fix: only stream when header is present

--- drive/api/files.py
+++ drive/api/files.py
@@ -597,7 +597,11 @@ def get_file_content(entity_name, trigger_download=0, jwt_token=None, transfer=F
 
 
 def get_file_internal(file, trigger_download=0):
-    if not trigger_download and get_file_type(file.as_dict() if file.as_dict else dict(file)) == "Video":
+    if (
+        not trigger_download
+        and get_file_type(file.as_dict() if file.as_dict else dict(file)) == "Video"
+        and frappe.request.headers.get("Range")
+    ):
         return stream_file_content(file.name)
     if file.document:
         frappe.local.response["type"] = "redirect"
