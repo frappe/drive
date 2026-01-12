6942ea79aa3014286d55886d6f69ccf2f4c611b3
Safwan Samsudeen <safwansamsudeen.c@gmail.com>
2026-01-12 11:42:37 +0530

    fix: share dialog upload issue

--- frontend/src/components/ShareDialog/AccessButton.vue
+++ frontend/src/components/ShareDialog/AccessButton.vue
@@ -1,45 +1,50 @@
 <template>
   <div class="flex gap-1">
-    <Dropdown
-      :button="{
-        label: shareAccess === 'editor' ? 'Can edit' : 'Can view',
-        iconRight: 'chevron-down',
-        variant: 'ghost',
-      }"
-      placement="right"
-      :options="[
-        {
-          value: 'reader',
-          label: 'Can view',
-          onClick: () => (shareAccess = 'reader'),
-          icon: shareAccess === 'reader' && 'check',
-        },
-        {
-          value: 'editor',
-          label: 'Can edit',
-          onClick: () => (shareAccess = 'editor'),
-          icon: shareAccess === 'editor' && 'check',
-        },
-        {
-          group: true,
-          hideLabel: true,
-          items: [
-            {
-              label: 'Remove',
-              onClick: () => emit('removeAccess'),
-              theme: 'red',
-            },
-          ],
-        },
-      ]"
-    />
+    <Dropdown :button="{
+      label: shareAccess === 'editor' ? 'Can edit' : (shareAccess === 'upload' ? 'Can upload' : 'Can view'),
+      iconRight: 'chevron-down',
+      variant: 'ghost',
+    }" placement="right" :options="dynamicList([
+      {
+        value: 'reader',
+        label: 'Can view',
+        onClick: () => (shareAccess = 'reader'),
+        icon: shareAccess === 'reader' && 'check',
+      },
+      {
+        value: 'editor',
+        label: 'Can edit',
+        onClick: () => (shareAccess = 'editor'),
+        icon: shareAccess === 'editor' && 'check',
+      },
+      {
+        value: 'upload',
+        label: 'Can upload',
+        cond: props.folder,
+        onClick: () => (shareAccess = 'upload'),
+        icon: shareAccess === 'upload' && 'check',
+      },
+      {
+        group: true,
+        hideLabel: true,
+        items: [
+          {
+            label: 'Remove',
+            onClick: () => emit('removeAccess'),
+            theme: 'red',
+          },
+        ],
+      },
+    ])" />
   </div>
 </template>
 <script setup>
 import { Dropdown } from "frappe-ui"
 import { ref, watch } from "vue"
+import { dynamicList } from "@/utils/files"
 
-const props = defineProps({ accessLevels: Object, accessObj: Object })
+
+const props = defineProps({ accessLevels: Object, accessObj: Object, folder: Boolean })
 const shareAccess = ref(props.accessObj.write ? "editor" : "reader")
 
 watch(shareAccess, (val) =>
--- frontend/src/components/ShareDialog/ShareDialog.vue
+++ frontend/src/components/ShareDialog/ShareDialog.vue
@@ -1,9 +1,5 @@
 <template>
-  <Dialog
-    v-model="open"
-    :options="{ size: 'lg' }"
-    @close="dialogType = ''"
-  >
+  <Dialog v-model="open" :options="{ size: 'lg' }" @close="dialogType = ''">
     <template #body-main>
       <div class="p-4 sm:px-6">
         <!-- Header -->
@@ -15,11 +11,7 @@
             </div>
             "
           </div>
-          <Button
-            class="ml-auto shrink-0"
-            variant="ghost"
-            @click="$emit('update:modelValue', false)"
-          >
+          <Button class="ml-auto shrink-0" variant="ghost" @click="$emit('update:modelValue', false)">
             <template #icon>
               <LucideX class="size-4" />
             </template>
@@ -33,157 +25,83 @@
             </div>
             <div class="flex justify-between mt-3">
               <div class="flex flex-col gap-2">
-                <Select
-                  v-model="generalAccessLevel"
-                  :options="levelOptions"
-                  @update:model-value="
-                    (val) => updateGeneralAccess(val, generalPerms)
-                  "
-                >
+                <Select v-model="generalAccessLevel" :options="levelOptions" @update:model-value="
+                  (val) => updateGeneralAccess(val, generalPerms)
+                ">
                   <template #prefix>
-                    <component
-                      :is="generalAccessLevel.icon"
-                      class="mr-2 size-4 text-ink-gray-6"
-                    />
+                    <component :is="generalAccessLevel.icon" class="mr-2 size-4 text-ink-gray-6" />
                   </template>
                   <template #item-prefix="{ option }">
-                    <component
-                      :is="option.icon"
-                      class="size-4 text-ink-gray-6"
-                    />
+                    <component :is="option.icon" class="size-4 text-ink-gray-6" />
                   </template>
                 </Select>
-                <TeamSelector
-                  v-if="generalAccessLevel == 'team'"
-                  v-model="chosenTeam"
-                />
+                <TeamSelector v-if="generalAccessLevel == 'team'" v-model="chosenTeam" />
               </div>
-              <Select
-                v-if="generalAccessLevel !== 'restricted'"
-                v-model="generalPerms"
-                :options="accessOptions"
+              <Select v-if="generalAccessLevel !== 'restricted'" v-model="generalPerms" :options="accessOptions"
                 @update:model-value="
                   (val) => updateGeneralAccess(generalAccessLevel, val)
-                "
-              />
+                " />
             </div>
           </div>
           <!-- Members section -->
           <div class="text-ink-gray-5 font-medium text-base mb-2">Members</div>
           <div class="flex gap-3">
             <div class="flex-grow">
-              <Combobox
-                v-slot="{ open }"
-                v-model="sharedUsers"
-                multiple
-              >
-                <div
-                  class="flex flex-col items-start justify-center rounded-md bg-surface-gray-2"
-                >
+              <Combobox v-slot="{ open }" v-model="sharedUsers" multiple>
+                <div class="flex flex-col items-start justify-center rounded-md bg-surface-gray-2">
                   <div class="flex flex-wrap justify-between p-1 w-full">
                     <div class="w-[75%] flex flex-wrap">
-                      <Button
-                        v-for="(user, idx) in sharedUsers"
-                        :key="user.name"
-                        :label="user.email"
-                        variant="outline"
-                        class="shadow-sm m-0.5 p-1"
-                      >
+                      <Button v-for="(user, idx) in sharedUsers" :key="user.name" :label="user.email" variant="outline"
+                        class="shadow-sm m-0.5 p-1">
                         <template #prefix>
-                          <Avatar
-                            size="sm"
-                            :image="user.image"
-                            :label="user.email"
-                          />
+                          <Avatar size="sm" :image="user.image" :label="user.email" />
                         </template>
                         <template #suffix>
-                          <LucideX
-                            class="h-4"
-                            stroke-width="1.5"
-                            @click.stop="() => sharedUsers.splice(idx, 1)"
-                          />
+                          <LucideX class="h-4" stroke-width="1.5" @click.stop="() => sharedUsers.splice(idx, 1)" />
                         </template>
                       </Button>
-                      <ComboboxInput
-                        ref="queryInput"
-                        v-focus
-                        placeholder="Add people..."
+                      <ComboboxInput ref="queryInput" v-focus placeholder="Add people..."
                         class="text-base px-1.5 p-1 flex-shrink min-w-24 grow basis-0 border-none bg-transparent 1 text-ink-gray-8 placeholder-ink-gray-4 focus:ring-0"
-                        autocomplete="off"
-                        @change="query = $event.target.value"
-                      />
+                        autocomplete="off" @change="query = $event.target.value" />
                     </div>
                     <div class="w-[25%] mt-auto" />
                   </div>
                 </div>
 
-                <div
-                  class="absolute z-[4] rounded-lg bg-surface-modal text-base shadow-2xl"
-                >
-                  <ComboboxOptions
-                    v-if="open && query.length"
-                    class="max-h-[15rem] overflow-y-auto px-1.5 py-1.5"
-                  >
-                    <ComboboxOption
-                      v-if="!filteredUsers.length"
-                      v-slot="{ selected, active }"
-                      as="template"
-                      :value="baseOption"
-                    >
-                      <li
-                        class="flex items-center justify-between rounded px-2.5 py-1.5 text-base text-ink-gray-7"
+                <div class="absolute z-[4] rounded-lg bg-surface-modal text-base shadow-2xl">
+                  <ComboboxOptions v-if="open && query.length" class="max-h-[15rem] overflow-y-auto px-1.5 py-1.5">
+                    <ComboboxOption v-if="!filteredUsers.length" v-slot="{ selected, active }" as="template"
+                      :value="baseOption">
+                      <li class="flex items-center justify-between rounded px-2.5 py-1.5 text-base text-ink-gray-7"
                         :class="{
                           'bg-surface-gray-3': active,
-                        }"
-                      >
-                        <span
-                          class="block truncate"
-                          :class="{
-                            'font-medium': selected,
-                            'font-normal': !selected,
-                          }"
-                        >
+                        }">
+                        <span class="block truncate" :class="{
+                          'font-medium': selected,
+                          'font-normal': !selected,
+                        }">
                           {{ query }}
                         </span>
                       </li>
                     </ComboboxOption>
-                    <template
-                      v-for="person in filteredUsers"
-                      :key="person.email"
-                    >
-                      <ComboboxOption
-                        v-slot="{ selected, active }"
-                        as="template"
-                        :value="person"
-                        :disabled="person.disabled"
-                      >
+                    <template v-for="person in filteredUsers" :key="person.email">
+                      <ComboboxOption v-slot="{ selected, active }" as="template" :value="person"
+                        :disabled="person.disabled">
                         <li
                           class="flex flex-1 gap-2 overflow-hidden items-center rounded px-2.5 py-1.5 text-base text-ink-gray-7"
                           :class="{
                             'bg-surface-gray-3': active,
                             'cursor-pointer': !person.disabled,
                             'opacity-50 cursor-not-allowed': person.disabled,
-                          }"
-                        >
-                          <LucideCheck
-                            v-if="selected"
-                            class="size-4 text-ink-gray-7"
-                          />
-                          <div
-                            v-else
-                            class="size-4"
-                          />
-                          <span
-                            class="block truncate"
-                            :class="{
-                              'font-medium': selected,
-                              'font-normal': !selected,
-                            }"
-                          >
+                          }">
+                          <LucideCheck v-if="selected" class="size-4 text-ink-gray-7" />
+                          <div v-else class="size-4" />
+                          <span class="block truncate" :class="{
+                            'font-medium': selected,
+                            'font-normal': !selected,
+                          }">
                             {{ person.email }}
-                            <span v-if="person.full_name"
-                              >({{ person.full_name }})</span
-                            >
+                            <span v-if="person.full_name">({{ person.full_name }})</span>
                           </span>
                         </li>
                       </ComboboxOption>
@@ -192,136 +110,82 @@
                 </div>
               </Combobox>
             </div>
-            <Select
-              v-model="shareAccess"
-              class="flex items-start min-w-36"
-              :options="
-                advancedTweak
-                  ? filteredAccess.map((k) => ({
-                      value: k,
-                      label: k[0].toUpperCase() + k.slice(1),
-                    }))
-                  : accessOptions
-              "
-            />
+            <Select v-model="shareAccess" class="flex items-start min-w-36" :options="advancedTweak
+                ? filteredAccess.map((k) => ({
+                  value: k,
+                  label: k[0].toUpperCase() + k.slice(1),
+                }))
+                : accessOptions
+              " />
           </div>
 
-          <div
-            v-if="getUsersWithAccess.data"
-            class="flex flex-col gap-4 overflow-y-auto text-base max-h-80 py-4 mb-3"
-          >
-            <div
-              v-for="(user, idx) in getUsersWithAccess.data"
-              :key="user.name"
-              class="flex items-center gap-x-3 pr-1"
-            >
-              <Avatar
-                size="xl"
-                :label="user.user || user.email"
-                :image="user.user_image"
-              />
+          <div v-if="getUsersWithAccess.data" class="flex flex-col gap-4 overflow-y-auto text-base max-h-80 py-4 mb-3">
+            <div v-for="(user, idx) in getUsersWithAccess.data" :key="user.name" class="flex items-center gap-x-3 pr-1">
+              <Avatar size="xl" :label="user.user || user.email" :image="user.user_image" />
 
               <div class="flex items-start flex-col gap-1">
                 <span class="font-medium text-base text-ink-gray-9">{{
                   user.full_name || user.user || user.email
-                }}</span>
+                  }}</span>
                 <span class="text-ink-gray-7 text-sm">{{
                   user.full_name ? user.user || user.email : ""
-                }}</span>
+                  }}</span>
               </div>
-              <span
-                v-if="user.user == $store.state.user.id"
-                class="ml-auto mr-1 text-ink-gray-7"
-              >
-                <div
-                  v-if="user.user === entity.owner"
-                  class="flex gap-1"
-                >
+              <span v-if="user.user == $store.state.user.id" class="ml-auto mr-1 text-ink-gray-7">
+                <div v-if="user.user === entity.owner" class="flex gap-1">
                   Owner (you)
                 </div>
                 <template v-else>You</template>
               </span>
-              <AccessButton
-                v-else-if="user.user !== entity.owner"
-                class="text-ink-gray-7 relative flex-shrink-0 ml-auto"
-                :access-obj="user"
-                :access-levels="filteredAccess"
-                @update-access="
+              <AccessButton v-else-if="user.user !== entity.owner"
+                class="text-ink-gray-7 relative flex-shrink-0 ml-auto" :access-obj="user"
+                :access-levels="filteredAccess" :folder="entity.is_group && true" @update-access="
                   (access) =>
                     updateAccess.submit({
                       entity_name: entity.name,
                       user: user.user,
                       ...access,
                     })
-                "
-                @remove-access="
+                " @remove-access="
                   getUsersWithAccess.data.splice(idx, 1),
-                    updateAccess.submit({
-                      method: 'unshare',
-                      entity_name: entity.name,
-                      user: user.user,
-                    })
-                "
-              />
-              <span
-                v-else
-                class="ml-auto flex items-center gap-1 text-ink-gray-5"
-              >
+                  updateAccess.submit({
+                    method: 'unshare',
+                    entity_name: entity.name,
+                    user: user.user,
+                  })
+                  " />
+              <span v-else class="ml-auto flex items-center gap-1 text-ink-gray-5">
                 Owner
                 <LucideDiamond class="size-3" />
               </span>
             </div>
           </div>
-          <div
-            v-else
-            class="flex min-h-[19.2vh] w-full"
-          >
+          <div v-else class="flex min-h-[19.2vh] w-full">
             <LoadingIndicator class="w-7 h-auto text-ink-gray-7 mx-auto" />
           </div>
           <div class="w-full flex items-center justify-between">
             <div>
-              <Button
-                class="text-sm"
-                variant="ghost"
-                label="Advanced"
-                :icon-left="h(LucideSettings, { class: 'size-4' })"
-                @click="advanced = true"
-              />
+              <Button class="text-sm" variant="ghost" label="Advanced"
+                :icon-left="h(LucideSettings, { class: 'size-4' })" @click="advanced = true" />
             </div>
             <div class="flex gap-2">
-              <Button
-                class="text-base"
-                variant="outline"
-                @click="getLink(entity)"
-              >
+              <Button class="text-base" variant="outline" @click="getLink(entity)">
                 <template #prefix>
                   <LucideLink2 class="w-4 text-ink-gray-6" />
                 </template>
                 Copy Link
               </Button>
-              <Button
-                v-if="sharedUsers.length"
-                label="Invite"
-                variant="solid"
-                @click="addShares"
-              />
+              <Button v-if="sharedUsers.length" label="Invite" variant="solid" @click="addShares" />
             </div>
           </div>
         </div>
         <div v-else>
-          <div
-            class="flex text-sm gap-1 items-center mb-3 cursor-pointer"
-            label="Back"
-            @click="advanced = false"
-          >
+          <div class="flex text-sm gap-1 items-center mb-3 cursor-pointer" label="Back" @click="advanced = false">
             <LucideArrowLeft class="size-4" />
             Back
           </div>
 
-          <Switch
-            v-model="allowDownload"
-            label="Allow download"
-          />
+          <Switch v-model="allowDownload" label="Allow download" />
         </div>
       </div>
     </template>
@@ -435,8 +299,8 @@ const getGeneralAccess = createResource({
     generalPerms.value = data.write
       ? "editor"
       : data.upload
-      ? "upload"
-      : "reader"
+        ? "upload"
+        : "reader"
   },
 })
 getGeneralAccess.fetch({ user: "Guest" })
