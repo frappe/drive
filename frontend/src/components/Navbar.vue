8723fb7bf0803879f29f8ecdf0835ad55465763c
Safwan Samsudeen <safwansamsudeen.c@gmail.com>
2026-06-15 16:43:26 +0530

    fix: allow download in navbar too

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -184,8 +184,7 @@ const defaultActions = computed(() => {
           icon: LucideDownload,
           isEnabled: () =>
             !isVirtual(rootEntity.value) &&
-            !['Link', 'Presentation', 'Document'].includes(rootEntity.value.file_type) &&
-            rootEntity.value.allow_download,
+            !['Link', 'Presentation', 'Document'].includes(rootEntity.value.file_type),
           onClick: () => entitiesDownload([rootEntity.value]),
         },
         {
